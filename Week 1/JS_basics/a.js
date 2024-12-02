8d5ea27e8345ffc5ad1121c7ae245a0b1f156026
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2024-12-02 20:39:20 +0530

    js files

---
+++ Week 1/JS_basics/a.js
@@ -0,0 +1,15 @@
+// Variables
+
+let name = "John";  // Variables that can be reassigned
+const age = 90; // constant variable, cannot be reassigned
+let isAdult = "True"; // 
+
+console.log(name)
+console.log(age)
+console.log(isAdult)
+
+let color = "red";
+let height = 190;
+let pizza = true;
+
+console.log(`My favourite color is ${color} , my height is ${height} and likePizza is ${pizza}`)
---
+++ Week 1/JS_basics/c.js
@@ -0,0 +1,15 @@
+// Operators
+
+let sum = 10 + 5;
+let diff = 90 - 8;
+let mul = 9 * 4;
+let div = 5 / 4;
+let exp = 8 ** 6;
+let mod = 9 % 4;
+
+console.log(sum);
+console.log(diff);
+console.log(mul);
+console.log(div);
+console.log(exp);
+console.log(mod);
