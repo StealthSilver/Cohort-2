5e25e76d245a0005444d5af305863ca655d81f11
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2024-12-03 11:41:32 +0530

    sync

---
+++ Week 2/07.js
@@ -0,0 +1,15 @@
+// making a synchronous set timeout
+
+function setTimeoutSync(timeout) {
+    let startTime = new Date();
+
+    while (1) {
+        let currrentTime = new Date();
+        if (currrentTime.getTime() - startTime.getTime() > timeout) {
+            break;
+        }
+    }
+}
+
+setTimeoutSync(1000);
+console.log("hi there")
