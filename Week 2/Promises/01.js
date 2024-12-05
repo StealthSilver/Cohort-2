91be18a7d9768df1ba7bbd522109f36dd50a399e
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2024-12-05 11:30:06 +0530

    classes

---
+++ Week 2/Promises/01.js
@@ -0,0 +1,21 @@
+class Rectangle {
+    constructor(width, height, color) {
+        this.width = width;
+        this.height = height;
+        this.color = color;
+    }
+
+    area() {
+        const area = this.width * this.height;
+        return area;
+    }
+
+    paint() {
+        console.log(`Painting with color ${this.color}`)
+    }
+}
+
+const rect1 = new Rectangle(2, 4, "red");
+const area = rect1.area();
+console.log(area)
+rect1.paint()
