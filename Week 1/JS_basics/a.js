13e9d2a2387a2ff8c1531385d22a896bcc639c77
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-01-27 19:34:22 +0530

    post2

--- Week 1/JS_basics/a.js
+++ Week 1/JS_basics/a.js
@@ -1,15 +1,17 @@
 // Variables
 
-let name = "John";  // Variables that can be reassigned
+let name = "John"; // Variables that can be reassigned
 const age = 90; // constant variable, cannot be reassigned
-let isAdult = "True"; // 
+let isAdult = "True"; // boolean variable, can be true or false
 
-console.log(name)
-console.log(age)
-console.log(isAdult)
+console.log(name);
+console.log(age);
+console.log(isAdult);
 
 let color = "red";
 let height = 190;
 let pizza = true;
 
-console.log(`My favourite color is ${color} , my height is ${height} and likePizza is ${pizza}`)
+console.log(
+  `My favourite color is ${color} , my height is ${height} and likePizza is ${pizza}`
+);
