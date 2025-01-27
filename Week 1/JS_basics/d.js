ce3864b3e63c2aeac7435f8e5b07f046377ce137
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-01-27 21:35:03 +0530

    img4

--- Week 1/JS_basics/d.js
+++ Week 1/JS_basics/d.js
@@ -1,39 +1,38 @@
 // functions
 
 function greet(name) {
-    return "hello " + name;
+  return "hello " + name;
 }
 
-let ans = greet("pogo")
-let ans2 = greet("groot")
-let ans3 = greet("micky")
+let ans = greet("pogo");
+let ans2 = greet("groot");
+let ans3 = greet("micky");
 
-console.log(ans)
-console.log(ans2)
-console.log(ans3)
+console.log(ans);
+console.log(ans2);
+console.log(ans3);
 
 function sum(a, b) {
-    return a + b;
+  return a + b;
 }
 
-let c = sum(3, 4)
-let d = sum(3, -4)
-let e = sum(343, 4)
+let c = sum(3, 4);
+let d = sum(3, -4);
+let e = sum(343, 4);
 
-console.log(c)
-console.log(d)
-console.log(e)
+console.log(c);
+console.log(d);
+console.log(e);
 
 console.log(4, "pogo");
 
 function canVote(age) {
-    if (age >= 18) {
-        console.log("can vote");
-    }
-    else {
-        console.log("cannot vote");
-    }
+  if (age >= 18) {
+    console.log("can vote");
+  } else {
+    console.log("cannot vote");
+  }
 }
 
-canVote(90)
-canVote(3)
+canVote(90);
+canVote(3);
