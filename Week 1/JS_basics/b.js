78f46c7606e2e1f4de22600e30939962608b2c9b
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-01-28 12:12:57 +0530

    img8

--- Week 1/JS_basics/b.js
+++ Week 1/JS_basics/b.js
@@ -4,3 +4,10 @@ let number = 34; // number
 let name = "pogo"; // string
 let isAdult = true; // boolean
 let color = ["red", "green", "yellow"]; //array
+
+//Object
+let country = {
+  name: "Japan",
+  capital: "Tokyo",
+  currency: "Yen",
+};
