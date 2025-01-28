fc068a9141e2ba6cffd8be76d8198cd2630a2a57
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-01-28 13:15:15 +0530

    img13

---
+++ Week 2/Async function/async.js
@@ -0,0 +1,23 @@
+function first() {
+  console.log("First task");
+}
+
+function second() {
+  console.log("Second task");
+}
+
+function third() {
+  console.log("Third task");
+}
+
+// Simulate an async operation using setTimeout
+function asyncTask() {
+  setTimeout(() => {
+    console.log("Async task finished");
+  }, 2000); // Waits 2 seconds before executing
+}
+
+first();
+asyncTask(); // This can run while the program moves on
+second();
+third();
---
+++ Week 2/Async function/sync.js
@@ -0,0 +1,10 @@
+function first() {
+  console.log("First task");
+}
+
+function second() {
+  console.log("Second task");
+}
+
+first(); // The program waits for this to finish
+second(); // Runs after first() finishes
