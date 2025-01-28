ddca34be044973c04cec3d77ca0c49316c1db1cc
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-01-28 13:28:28 +0530

    img13

--- Week 2/Async function/07.js
+++ Week 2/Async function/07.js
@@ -1,16 +1,16 @@
 // making a synchronous set timeout
 
 function setTimeoutSync(timeout) {
-    let startTime = new Date();
+  let startTime = new Date();
 
-    while (1) {
-        let currrentTime = new Date();
-        if (currrentTime.getTime() - startTime.getTime() > timeout) {
-            break;
-        }
+  while (1) {
+    let currrentTime = new Date();
+    if (currrentTime.getTime() - startTime.getTime() > timeout) {
+      console.log("no hi there until i finish :)");
+      break;
     }
+  }
 }
 
 setTimeoutSync(1000);
-console.log("hi there")
-
+console.log("hi there");
