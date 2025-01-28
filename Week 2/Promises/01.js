6c3510c32fc665879efd72354352837845a0af57
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-01-28 14:38:45 +0530

    img13

--- Week 2/Promises/01.js
+++ Week 2/Promises/01.js
@@ -1,22 +1,23 @@
 class Rectangle {
-    constructor(width, height, color) {
-        this.width = width;
-        this.height = height;
-        this.color = color;
-    }
+  constructor(width, height, color) {
+    this.width = width;
+    this.height = height;
+    this.color = color;
+  }
 
-    area() {
-        const area = this.width * this.height;
-        return area;
-    }
+  area() {
+    const area = this.width * this.height;
+    return area;
+  }
 
-    paint() {
-        console.log(`Painting with color ${this.color}`)
-    }
+  paint() {
+    console.log(`Painting with color ${this.color}`);
+  }
 }
-
 
 const rect1 = new Rectangle(2, 4, "red"); // creating a new object of the rectangle class
 const area = rect1.area();
-console.log(area)
-rect1.paint()
+
+console.log(rect1);
+console.log(area);
+rect1.paint();
