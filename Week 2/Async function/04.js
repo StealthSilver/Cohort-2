39d8e108bd9fe15c784f2c9fded191f97e537187
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-01-28 13:22:43 +0530

    img13

--- Week 2/Async function/04.js
+++ Week 2/Async function/04.js
@@ -1,19 +1,16 @@
-// asynchronous execution
+// asynchronous execution of fs.readfile
 
 const fs = require("fs");
 
 function print(err, data) {
-    if (err) {
-        console.log("file not found")
-    }
-    else {
-        console.log(data);
-    }
-
+  if (err) {
+    console.log("file not found");
+  } else {
+    console.log(data);
+  }
 }
 
 fs.readFile("a.txt", "utf-8", print); // asynchronous code
 fs.readFile("b.txt", "utf-8", print); // asynchronous code
 
-console.log("done")
-
+console.log("done");
