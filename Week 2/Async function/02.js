d146b63e0c174e665d5a137116011b6012dff9ec
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-01-28 13:17:31 +0530

    img13

--- Week 2/Async function/02.js
+++ Week 2/Async function/02.js
@@ -3,7 +3,5 @@ const fs = require("fs");
 const contents = fs.readFileSync("a.txt", "utf-8"); // synchronous reading
 console.log(contents);
 
-
 const contents2 = fs.readFileSync("b.txt", "utf-8"); // synchronous reading
 console.log(contents2);
-
