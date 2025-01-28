42931aca3f293d9137fc25a62dc87002dfa84a94
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-01-28 14:44:22 +0530

    img13

--- Week 2/Promises/02.js
+++ Week 2/Promises/02.js
@@ -7,9 +7,9 @@ console.log(date.getMonth());
 console.log(date.getYear());
 console.log(date.getFullYear());
 
-
 // Map class
 const map = new Map();
 map.set("name", "alice");
 map.set("age", 40);
-console.log(map.get("name"))
+console.log(map.get("name"));
+console.log(map.get("age"));
