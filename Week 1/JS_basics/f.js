d529ca02f7a1809737b35f3d3e1a175fb5f2b53d
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-01-28 12:36:52 +0530

    img11

--- Week 1/JS_basics/f.js
+++ Week 1/JS_basics/f.js
@@ -3,6 +3,5 @@
 let users = ["groot", "ironman", "captain", "strange"];
 
 for (let i = 0; i < users.length; i++) {
-    console.log(users[i]);
+  console.log(users[i]);
 }
-
--- Week 1/JS_basics/h.js
+++ Week 1/JS_basics/h.js
@@ -1,33 +1,32 @@
-
 function greet(user) {
-    let des;
-    let gender = user.gender;
-    let age = user.age;
+  let des;
+  let gender = user.gender;
+  let age = user.age;
 
-    if (gender == "male") {
-        des = "Mr";
-    } else if (gender == "female") {
-        des = "Ms";
-    } else {
-        des = "";
-    }
-    let canVote = true;
+  if (gender == "male") {
+    des = "Mr";
+  } else if (gender == "female") {
+    des = "Ms";
+  } else {
+    des = "";
+  }
+  let canVote = true;
 
-    if (age >= 18) {
-        canVote = true
-    }
-    else {
-        canVote = false
-    }
+  if (age >= 18) {
+    canVote = true;
+  } else {
+    canVote = false;
+  }
 
-    console.log(`Hello ${des} ${user.name}, your age is ${user.age}. and the voting ability is ${canVote}`);
+  console.log(
+    `Hello ${des} ${user.name}, your age is ${user.age}. and the voting ability is ${canVote}`
+  );
 }
 
 let user = {
-    name: "pogo",
-    age: 9,
-    gender: "male"
+  name: "pogo",
+  age: 9,
+  gender: "male",
 };
 
 greet(user);
-
