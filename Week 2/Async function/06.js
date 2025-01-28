9deefc2f656a76656a5f227df64f8eeed1af1b93
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-01-28 13:25:28 +0530

    img13

--- Week 2/Async function/06.js
+++ Week 2/Async function/06.js
@@ -1,17 +1,18 @@
 function timeout() {
-    console.log("click the button")
+  console.log("timeout complete");
 }
 
 console.log("hi");
 
 setTimeout(timeout, 1000);
 
-console.log("welcome to loupe")
+console.log("set timeout still running");
 
 let c = 0;
 
-for (let i = 0; i < 1000000000; i++) {  // CPU intensive task
-    c = c + 1;
+for (let i = 0; i < 1000000000; i++) {
+  // CPU intensive task
+  c = c + 1;
 }
 
-console.log("expensive opn done")
+console.log("expensive opn done");
