abd3ebaaed591a590fb84690b847578ab66d1a14
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-01-28 16:08:39 +0530

    img13

--- Week 2/Promises/06.js
+++ Week 2/Promises/06.js
@@ -1,20 +1,20 @@
 // promisified read file
 
-const fs = require('fs');
+const fs = require("fs");
 
-function readFilePromisified(filePath, encoding = 'utf8') {
-    return new Promise((resolve, reject) => {
-        fs.readFile(filePath, encoding, (err, data) => {
-            if (err) {
-                reject(err); // Reject the promise on error
-            } else {
-                resolve(data); // Resolve the promise with the file data
-            }
-        });
+function readFilePromisified(filePath, encoding = "utf8") {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, encoding, (err, data) => {
+      if (err) {
+        reject(err); // Reject the promise on error
+      } else {
+        resolve(data); // Resolve the promise with the file data
+      }
     });
+  });
 }
 
 // using
-readFilePromisified('01.txt')
-    .then(data => console.log(data))
-    .catch(err => console.error('Error:', err));
+readFilePromisified("01.txt")
+  .then((data) => console.log(data))
+  .catch((err) => console.error("Error:", err));
