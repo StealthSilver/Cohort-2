f7aa2ebb867cc032775bb82586351794ad7ebd3f
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-01-28 16:08:33 +0530

    img13

--- Week 2/Promises/05.js
+++ Week 2/Promises/05.js
@@ -1,15 +1,12 @@
 function random(resolve) {
-    setTimeout(resolve, 3000)
+  setTimeout(resolve, 3000);
 }
 
 let p = new Promise(random); // supposed to return a promise
 
-
-
-
 // using the eventual value retured by the promise class
 function callback() {
-    console.log("promise succeded");
+  console.log("promise succeded");
 }
 
 p.then(callback);
