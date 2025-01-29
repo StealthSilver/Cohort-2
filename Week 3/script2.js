751ea63ca2d908557d9598888ca96b5ad8fc9d63
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-01-29 13:08:02 +0530

    inti

--- Week 3/script2.js
+++ Week 3/script2.js
@@ -6,5 +6,11 @@ function addTodo() {
   // creating and appending a new div
   const newDivEl = document.createElement("div");
   newDivEl.innerHTML = value;
+
   document.querySelector("body").appendChild(newDivEl);
+
+  // adding and appending the delete button
+  const DelBtn = document.createElement("button");
+  DelBtn.innerHTML = value;
+  document.querySelector("body").appendChild(DelBtn);
 }
