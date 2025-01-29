32a7defa93800161e61f718015848964f745e0c4
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-01-29 13:21:01 +0530

    inti

--- Week 3/script2.js
+++ Week 3/script2.js
@@ -8,17 +8,21 @@ function deleteTodo(index) {
 
 function addTodo() {
   // getting the input element
+
   const inputEl = document.querySelector("input");
   const value = inputEl.value;
 
   // creating and appending a new div
+  // adding and appending the delete button
   const newDivEl = document.createElement("div");
-  newDivEl.innerHTML = value;
+  newDivEl.setAttribute("id", "todo-" + ctr);
+  newDivEl.innerHTML =
+    "<div>" +
+    value +
+    '<div><button onclick="deleteTodo(' +
+    ctr +
+    ')">delete</button>';
 
   document.querySelector("body").appendChild(newDivEl);
-
-  // adding and appending the delete button
-  const DelBtn = document.createElement("button");
-  DelBtn.innerHTML = value;
-  document.querySelector("body").appendChild(DelBtn);
+  ctr = ctr + 1;
 }
