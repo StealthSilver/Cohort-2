fd633d21cd4ee30066246cda643bfe44e6eb6092
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-01-29 13:00:18 +0530

    inti

---
+++ Week 3/script2.js
@@ -0,0 +1,7 @@
+function addTodo() {
+  const inputEl = document.querySelector("input"); // getting the input element
+  const value = inputEl.value;
+
+  const newDivEl = document.createElement("div");
+  newDivEl.innerHTML = value;
+}
