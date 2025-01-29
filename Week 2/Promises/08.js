1ac4c4b03e42b9293c4db0ed70deb781b0f9baaa
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-01-29 11:50:57 +0530

    inti

---
+++ Week 2/Promises/08.js
@@ -0,0 +1,13 @@
+// this is the callback hell
+
+setTimeout(function () {
+  console.log("hi");
+  setTimeout(function () {
+    console.log("hello");
+    setTimeout(function () {
+      console.log("hello there");
+    }, 5000);
+  }, 3000);
+}, 1000);
+
+console.log("outside the callback hell");
