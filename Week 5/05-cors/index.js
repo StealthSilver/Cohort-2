24839cf0874210dfe40da032cb5f83007ae99491
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-01-30 23:33:25 +0530

    week 3

--- Week 5/05-cors/index.js
+++ Week 5/05-cors/index.js
@@ -1,9 +1,14 @@
 // using cors
 
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
+
 app.use(express.json());
+
+app.use(cors());
+
 app.post("/sum", function (req, res) {
   const a = parseInt(req.body.a);
   const b = parseInt(req.body.b);
