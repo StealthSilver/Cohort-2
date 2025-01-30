4a2c67cd9a2ed60a5002ee60ab08a8ea317cad69
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-01-30 22:04:42 +0530

    week 3

--- Week 5/04-Middlewares/index.js
+++ Week 5/04-Middlewares/index.js
@@ -4,8 +4,8 @@ const app = express();
 // better routing and database, middlewares
 
 app.get("/sum", function (req, res) {
-  const a = parseInt(req, query.a);
-  const b = parseInt(req, query.b);
+  const a = parseInt(req.query.a);
+  const b = parseInt(req.query.b);
 
   res.json({
     ans: a + b,
