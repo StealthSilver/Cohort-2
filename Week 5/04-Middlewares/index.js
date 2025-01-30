452ced03c18700ca5216f86c2d292559956370b2
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-01-30 22:03:20 +0530

    week 3

--- Week 5/04-Middlewares/index.js
+++ Week 5/04-Middlewares/index.js
@@ -1,2 +1,15 @@
 const express = require("express");
 const app = express();
+
+// better routing and database, middlewares
+
+app.get("/sum", function (req, res) {
+  const a = parseInt(req, query.a);
+  const b = parseInt(req, query.b);
+
+  res.json({
+    ans: a + b,
+  });
+});
+
+app.listen(3000);
