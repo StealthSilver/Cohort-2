91ed917bf2d807c2b48ab5694f76e1a658159b2a
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-01-30 17:15:14 +0530

    middlewares

---
+++ Week 5/http-server/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+app.get("/sum", function (req, res) {});
+
+app.get("/dif", function (req, res) {});
+
+app.get("/mul", function (req, res) {});
+
+app.get("/div", function (req, res) {});
+
+app.listen();
