48692bf235159b5054d0ffffa021ac74b2db08da
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-01-30 12:40:41 +0530

    inti

--- Week 4/cli/index.js
+++ Week 4/cli/index.js
@@ -1,29 +1,27 @@
 const fs = require("fs");
-
 const { Command } = require("commander");
 const program = new Command();
 
 program
   .name("counter")
-  .description("CLI to do file based tasks")
+  .description("CLI to do file-based tasks")
   .version("0.8.0");
 
 program
   .command("count")
-  .description("count the numebr of words in a file")
-  .argument("<file>", "file to count")
+  .description("Count the number of words in a file") // Fixed typo
+  .argument("<file>", "File to count words from")
   .action((file) => {
     fs.readFile(file, "utf-8", (err, data) => {
       if (err) {
-        console.log(err);
-      } else {
-        let words = 0;
-        for (let i = 0; i < data.length; i++) {
-          if (data[i] === " ") {
-            words++;
-          }
-        }
-        console.log(`There are ${words + 1} words in ${file}`);
+        console.error("Error reading file:", err.message);
+        return;
       }
+
+      // Corrected word count logic
+      const words = data.trim().split(/\s+/).length;
+      console.log(`There are ${words} words in ${file}`);
     });
   });
+
+program.parse(process.argv);
