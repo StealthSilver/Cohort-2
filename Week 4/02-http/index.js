4e82d0ac6a70d60271e5b465fe24d0c6a872f16f
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-01-30 14:03:22 +0530

    init

--- Week 4/02-http/index.js
+++ Week 4/02-http/index.js
@@ -2,8 +2,10 @@ const express = require("express");
 
 const app = express();
 
+// request handlers
 app.get("/", function (req, res) {
   res.send("hello world");
 });
 
+// listning on the port
 app.listen(3000);
