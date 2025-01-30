ee92c6869c7b530fb3fc6a38a3f1fd9bd87a82fa
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-01-30 17:54:34 +0530

    middlewares

--- Week 5/helper-functions/03.js
+++ Week 5/helper-functions/03.js
@@ -1,3 +1,16 @@
 // filter function
 
 // given an input array return all the even values
+
+const input = [1, 2, 3, 4, 5, 6];
+
+function transform(n) {
+  if (n % 2 == 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+const ans = input.filter(transform);
+console.log(ans);
