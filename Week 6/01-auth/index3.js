db960de071e2ee2c1029369ccd052c99d92402f2
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-01-31 12:08:12 +0530

    auth

---
+++ Week 6/01-auth/index3.js
@@ -0,0 +1,82 @@
+// creating a token and asking for user's data
+
+const express = require("express");
+
+const app = express();
+
+// this middleware will let us parse the post body
+app.use(express.json());
+
+// creating a in memory variable (database)
+const users = [];
+
+// generating token using a random string
+
+app.post("/signup", function (req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  users.push({
+    username: username,
+    password: password,
+  });
+
+  res.json({
+    message: "you are signed up",
+  });
+});
+
+app.post("/signin", function (req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  let foundUser = null;
+
+  for (let i = 0; i < users.length; i++) {
+    if (users[i].username == username && users[i].password == password) {
+      foundUser = users[i];
+    }
+  }
+
+  //   const user = users.find(function (u) {
+  //     if (u.username == username) {
+  //       return true;
+  //     } else {
+  //       return false;
+  //     }
+  //   });
+
+  if (foundUser) {
+    const token = generateToken();
+    foundUser.token = token;
+    res.json({
+      message: token,
+    });
+  }
+});
+
+// sending the token to the server in the header
+app.get("/me", function (req, res) {
+  const token = req.headers.token;
+
+  let foundUser = null;
+
+  for (let i = 0; i < users.length; i++) {
+    if (users[i].token == token) {
+      foundUser = users[i];
+    }
+  }
+
+  if (foundUser) {
+    res.json({
+      username: foundUser.username,
+      password: foundUser.password,
+    });
+  } else {
+    res.json({
+      message: "token invalid",
+    });
+  }
+});
+
+app.listen(3000);
