176de274e854289fae8ba9138b781e246e8e7c37
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-01-31 11:02:04 +0530

    auth

--- Week 6/01-auth/index.js
+++ Week 6/01-auth/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 
 const app = express();
 
+// this middleware will let us parse the post body
 app.use(express.json());
 
 app.post("/signup", function (req, res) {});
