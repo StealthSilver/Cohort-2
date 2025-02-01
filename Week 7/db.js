6afff81d7a29a4209b310dae6886f42a821ac43b
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-02-01 19:01:19 +0530

    auth

--- Week 7/db.js
+++ Week 7/db.js
@@ -1,5 +1,17 @@
 const mongoose = require("mongoose");
-
+const ObjectId = mongoose.ObjectId;
 // schema the database
 
-const Schema = mongoose.Schema();
+const Schema = mongoose.Schema;
+
+const User = new Schema({
+  email: String,
+  password: String,
+  name: String,
+});
+
+const Todo = new Schema({
+  title: String,
+  done: Boolean,
+  userId: ObjectId,
+});
