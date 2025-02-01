ab017953a0b2a1e6e35577b31e122ad376d50c4a
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-02-01 13:32:54 +0530

    auth

--- Week 7/index.js
+++ Week 7/index.js
@@ -1,10 +1,22 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const app = express();
-
 app.use(bodyParser.json());
+
+// MongoDB connection
+mongoose.connect("mongodb://localhost:27017/", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "Connection error:"));
+db.once("open", () => {
+  console.log("Connected to MongoDB");
+});
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
