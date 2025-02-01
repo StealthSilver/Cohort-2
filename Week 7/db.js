5ae2d6e808be505071b50fa886eee1d9737cad96
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-02-01 18:59:37 +0530

    auth

--- Week 7/db.js
+++ Week 7/db.js
@@ -1,3 +1,5 @@
 const mongoose = require("mongoose");
 
 // schema the database
+
+const Schema = mongoose.Schema();
--- Week 7/index.js
+++ Week 7/index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
 const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
 
 const app = express();
 app.use(bodyParser.json());
