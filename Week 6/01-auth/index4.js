4df68499e404c8496f7eed17a4f597560ad181dc
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-02-01 11:02:35 +0530

    auth

--- Week 6/01-auth/index4.js
+++ Week 6/01-auth/index4.js
@@ -60,17 +60,20 @@ app.post("/signin", function (req, res) {
 // auth as a middleware
 
 function auth(req, res, next) {
-  const token = req.header.token;
-  const decodedData = jwt.verify(token, JWT_SECRET);
-
-  if (decodedData.username) {
-    req.username = decodedData.username;
-    next();
-  } else {
-    res.json({
-      message: "you are not logged in",
-    });
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).json({ error: "Authorization header missing" });
   }
+
+  const token = authHeader.split(" ")[1]; // Assuming "Bearer <token>"
+
+  jwt.verify(token, JWT_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(403).json({ error: "Invalid token" });
+    }
+    req.user = decoded; // Attach user data to the request
+    next();
+  });
 }
 
 // auth middleware has to pass data
