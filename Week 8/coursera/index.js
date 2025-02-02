cb67839b5992a7ecd5a02b69938918f4242837f8
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-02-02 16:01:39 +0530

    coursers

--- Week 8/coursera/index.js
+++ Week 8/coursera/index.js
@@ -1,4 +1,28 @@
 const express = require("express");
 const app = express();
 
+app.post("/user/signup", function (req, res) {
+  res.json({
+    message: "signup endpoint",
+  });
+});
+
+app.post("/user/signin", function (req, res) {
+  res.json({
+    message: "signin endpoint",
+  });
+});
+
+app.get("/courses", function (req, res) {
+  res.json({
+    message: "list of courses",
+  });
+});
+
+app.get("/user/puchases", function (req, res) {
+  res.json({
+    message: "purchased courses",
+  });
+});
+
 app.listen(3000);
