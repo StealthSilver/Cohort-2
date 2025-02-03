28f1a0d24d85cb7276e22e2f7a25e435b31ca959
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-02-03 10:54:42 +0530

    coursera

--- Week 8/coursera/routes/user.js
+++ Week 8/coursera/routes/user.js
@@ -1,43 +1,37 @@
 const { Router } = require("express");
 
 const userRouter = Router();
-
 const { userModel } = require("../db");
+const bcrypt = require("bcrypt");
 
 // Signup route
 userRouter.post("/signup", async function (req, res) {
   const { email, password, firstName, lastName } = req.body;
-  // add zod validations
-  // hash the passwords, use bcrypt
+  // use zod for input validation
+  // Hash the password
+  const hashedPassword = await bcrypt.hash(password, 10);
 
   try {
     await userModel.create({
       email: email,
-      password: password,
+      password: hashedPassword,
       firstName: firstName,
       lastName: lastName,
     });
+    res.json({ message: "Signup successful" });
   } catch (e) {
-    return res.json({ message: "User already exists" });
+    return res.status(400).json({ message: "User already exists" });
   }
-
-  res.json({
-    message: "signup endpoint",
-  });
 });
 
 // Signin route
 userRouter.post("/signin", function (req, res) {
-  res.json({
-    message: "signin endpoint",
-  });
+  res.json({ message: "signin endpoint" });
 });
 
 // Getting the purchased courses (fixed typo in '/purchases')
 userRouter.get("/purchases", function (req, res) {
-  res.json({
-    message: "purchased courses",
-  });
+  res.json({ message: "purchased courses" });
 });
 
 module.exports = {
