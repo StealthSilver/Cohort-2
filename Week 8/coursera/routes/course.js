aff2eabf96dc2216f2728e90846da1277c2e9142
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-02-04 10:26:17 +0530

    coursera

--- Week 8/coursera/routes/course.js
+++ Week 8/coursera/routes/course.js
@@ -2,12 +2,21 @@ const { Router } = require("express");
 
 const courseRouter = Router();
 
-const { courseModel } = require("../db");
+const { courseModel, purchaseModel } = require("../db");
+const { userMiddleware } = require("../middleware/user");
+const { purchaseModel } = require("../db");
 
 // Purchase some course (fixed missing '/')
-courseRouter.post("/purchase", function (req, res) {
+courseRouter.post("/purchase", userMiddleware, async function (req, res) {
+  const userId = req.userId;
+  const courseId = req.body.courseId;
+
+  await purchaseModel.create({
+    userId,
+    courseId,
+  });
   res.json({
-    message: "purchasing the course",
+    message: "you have successfully bought the course",
   });
 });
 
