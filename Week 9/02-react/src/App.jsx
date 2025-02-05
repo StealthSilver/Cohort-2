7c4a2e905f890634215cb28aa7db7d4878a2fffd
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-02-05 11:13:41 +0530

    react

--- Week 9/02-react/src/App.jsx
+++ Week 9/02-react/src/App.jsx
@@ -17,24 +17,15 @@ function Counter() {
   // using the use state hook and initializing the state
   const [count , setCount] = useState(0);
 
-  function increaseCount(){
-   setCount(count + 1);
-  }
+ setInterval(function(){
+  setCount(count + 1);
 
-  function decreaseCount(){
-    setCount(count - 1);
-  }
-
-  function resetCount(){
-    setCount(0);
-  }
+ }, 1000)
 
   return (
     <div>
       <h1>{count}</h1>
-      <button onClick  = {increaseCount}>Increase Count</button> <br />
-      <button onClick  = {decreaseCount}>Decrease Count</button> <br />
-      <button onClick  = {resetCount}>Reset Count</button>
+      
     </div>
   );
 }
