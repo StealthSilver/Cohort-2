7d4570f68c63b79a82047b3dd85d344da2077f00
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-02-06 20:38:13 +0530

    react

--- Week 9/11-react/src/App.jsx
+++ Week 9/11-react/src/App.jsx
@@ -1,23 +1,17 @@
-
-
-
 function App() {
- 
-
   return (
-  <div>
-    <Card innerContent = {"hi there"}/>
-  </div>
-  )
-
-
+    <div>
+      <Card innerContent={"hi there"} />
+    </div>
+  );
 }
 
-function Card({innerContent}){
-  return <span style = {{background: "black", borderRadius: 10, color: "white"}}>{innerContent}</span>
+function Card({ innerContent }) {
+  return (
+    <span style={{ background: "black", borderRadius: 10, color: "white" }}>
+      {innerContent}
+    </span>
+  );
 }
 
-
-
-
-export default App
+export default App;
