1de8576749403e35edde0e1110d10748a705720c
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-02-06 14:06:12 +0530

    react

--- Week 9/04-react/src/App.jsx
+++ Week 9/04-react/src/App.jsx
@@ -5,6 +5,8 @@ function App() {
 
 function PostComponent(){
   return <div style = {{width : 200 , backgroundColor : "white", borderRadius :10 , borderColor: "gray" , borderWidth: 1}}>
+
+    <img src={"https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQwN0-cLq3vtAkGpSg6upZYLjRAbcu6ljrc5RZoXZkmoQObNT7szAUBgMCq8w6wMKDTNtvYb-4UFDGc470xr_BgCA"}></img>
     </div>
 }
 
