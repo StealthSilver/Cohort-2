3e8aac3af8e026b0679ec9b6f3211d20019d9924
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-02-06 14:10:54 +0530

    react

--- Week 9/04-react/src/App.jsx
+++ Week 9/04-react/src/App.jsx
@@ -6,7 +6,10 @@ function App() {
 
 
 function PostComponent(){
-  return <div style = {{width : 200 , backgroundColor : "white", borderRadius :10 , borderColor: "gray" , borderWidth: 1, display: "flex"}}>
+  return
+  
+  <div>
+    <div style = {{width : 200 , backgroundColor : "white", borderRadius :10 , borderColor: "gray" , borderWidth: 1, display: "flex"}}>
 
     <img src={"https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQwN0-cLq3vtAkGpSg6upZYLjRAbcu6ljrc5RZoXZkmoQObNT7szAUBgMCq8w6wMKDTNtvYb-4UFDGc470xr_BgCA"} style ={{
       width: 25 , height: 25, borderRadius : 20
@@ -15,6 +18,11 @@ function PostComponent(){
     <div>
       <b>GADHA</b>
       <div>23,482 followers</div>
+      <div>12m</div>
+    </div></div>
+
+    <div>
+      Want to know how to win big? check out how these folks won $6000 in bounties
     </div>
     </div>
 }
