ce99fff592957e329be5ee874894cb95ac15e0d2
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-02-06 18:07:33 +0530

    react

--- Week 9/05-react/src/App.jsx
+++ Week 9/05-react/src/App.jsx
@@ -1,11 +1,25 @@
+import {useState} from "react";
+
 
 function App() {
  
 
   return (
-    <>
-     
-    </>
+    <div style = {{background: "#dfe6e9" , height: "100vh"}}>
+      <ToggleMessage></ToggleMessage>
+    </div>
+  )
+}
+
+const ToggleMessage = () => {
+  const [isVisible , setIsVisible] = useState(false);
+
+
+  return(
+    <div>
+      <button oClick = {()=> setIsVisible(!isVisible)}>Toggle Message</button>
+      {isVisible && <p>This message is conditionally rendered</p>}
+    </div>
   )
 }
 
