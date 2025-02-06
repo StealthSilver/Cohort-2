1e44f8ef60b584309cba0169bd106caf9dffe93d
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-02-06 20:12:14 +0530

    react

--- Week 9/08-react/src/App.jsx
+++ Week 9/08-react/src/App.jsx
@@ -2,30 +2,36 @@ import {useEffect , useState} from "react";
 
 function App() {
   const [count, setCount] = useState(1)
+  const [count2, setCount2] = useState(1)
 
 function increaseCount(){
   setCount (currentValue => currentValue + 1)
 }
 
+function decreaseCount(){
+  setCount2(currentValue => currentValue -1)
+}
+
 // this code will only run on mount and not on every render
 useEffect(()=> {
-  console.log("above setinterval")
+  
   setInterval(increaseCount, 1000);
+  setInterval(decreaseCount, 1000);
 }, []) 
 // this effect will run only on mount, because the array is empty
 
 
 
 useEffect(()=> {
-  console.log("the count has been updated to" + count)
-  setInterval(increaseCount, 1000);
-}, [count]) 
-// this effect will run every time the count changes as there is the variable in the dependency array
+  console.log("the count has been updated to " + count)
+  
+}, [count ,count2]) 
+// this effect will run every time the count2 changes as there is the variable in the dependency array
 
 
   return (
     <div>
-      {count}
+      {count} {count2}
     </div>
   )
 }
