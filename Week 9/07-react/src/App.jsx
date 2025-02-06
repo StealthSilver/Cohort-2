3129656ac59c62cb0b70bbba3761aaa659d3d858
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-02-06 19:53:11 +0530

    react

--- Week 9/07-react/src/App.jsx
+++ Week 9/07-react/src/App.jsx
@@ -3,10 +3,14 @@ import {useEffect} from "react";
 function App() {
   const [count, setCount] = useState(0)
 
+function increaseCount(){
+  setCount (count +1)
+}
+
   return (
-    <>
-     
-    </>
+    <div>
+      {count}
+    </div>
   )
 }
 
