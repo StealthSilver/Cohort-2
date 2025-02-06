2c144b62b076754651b78b2ba9e32d2231dd7f46
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-02-06 20:38:02 +0530

    react

--- Week 9/11-react/src/App.jsx
+++ Week 9/11-react/src/App.jsx
@@ -1,15 +1,23 @@
-import { useState } from 'react'
+
 
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
-    <>
-      
-      
-    </>
+  <div>
+    <Card innerContent = {"hi there"}/>
+  </div>
   )
+
+
 }
+
+function Card({innerContent}){
+  return <span style = {{background: "black", borderRadius: 10, color: "white"}}>{innerContent}</span>
+}
+
+
+
 
 export default App
