199677e41578efb908413b7ab9b610195f8fd52e
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-02-06 13:10:02 +0530

    react

--- Week 9/03-react/src/App.jsx
+++ Week 9/03-react/src/App.jsx
@@ -1,12 +1,27 @@
+import { useState, useEffect } from "react"
+
+
 
 function App() {
   
 
   return (
-    <>
-      
-    </>
+    <div>
+      <Counter></Counter>
+    </div>
   )
+
+
+}
+
+function Counter(){
+
+  useEffect(function(){
+    console.log("render")
+  }, []);
+
+
+  return <div>Counter </div>
 }
 
 export default App
