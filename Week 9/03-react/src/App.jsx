8a46b4791b5d3633ecd073487f2071485e53660d
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-02-06 13:22:55 +0530

    react

--- Week 9/03-react/src/App.jsx
+++ Week 9/03-react/src/App.jsx
@@ -1,22 +1,16 @@
-import { useState, useEffect } from "react"
-
-
+import { useState, useEffect } from "react";
 
 function App() {
-
-
   const [count, setCount] = useState(0);
   const [count2, setCount2] = useState(0);
 
   function increase() {
-    setCount(c => c + 1)
+    setCount((c) => c + 1);
   }
 
   function decrease() {
-    setCount2(c => c - 1)
+    setCount2((c) => c - 1);
   }
-
-
 
   return (
     <div>
@@ -24,32 +18,31 @@ function App() {
       <button onClick={increase}>Increase Count</button>
       <button onClick={decrease}>Decrease Count</button>
     </div>
-  )
-
-
+  );
 }
 
 function Counter(props) {
-
-
-
   useEffect(function () {
-
-    console.log("mount")
+    console.log("mount");
 
     return function () {
-      console.log("unmount")
-    }
-
+      console.log("unmount");
+    };
   }, []);
 
-  useEffect(function () {
-    console.log("count has changed")
-  }, [props.count])
+  useEffect(
+    function () {
+      console.log("count has changed");
+    },
+    [props.count]
+  );
 
-
-  return <div>Counter 1 {props.count} <br />
-    Counter 2 {props.count2} </div>
+  return (
+    <div>
+      Counter 1 {props.count} <br />
+      Counter 2 {props.count2}{" "}
+    </div>
+  );
 }
 
-export default App
+export default App;
