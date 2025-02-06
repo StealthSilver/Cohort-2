2b18d11635fa9d91d9a91934380d03379fb7e7b7
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-02-06 19:54:52 +0530

    react

--- Week 9/07-react/src/App.jsx
+++ Week 9/07-react/src/App.jsx
@@ -1,11 +1,13 @@
-import {useEffect} from "react";
+import {useEffect , useState} from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(1)
 
 function increaseCount(){
   setCount (count +1)
 }
+
+setInterval(increaseCount, 1000);
 
   return (
     <div>
