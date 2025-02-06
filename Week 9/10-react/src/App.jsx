c79a0591d77899853e328e90d8f151f024c1b03d
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-02-06 20:34:11 +0530

    react

--- Week 9/10-react/src/App.jsx
+++ Week 9/10-react/src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
 const Greeting = ({ name }) => {
-    return <h1>Hello, {name}!</h1>;
+    return <h1>Hello, {name}!</h1>; 
 };
 
 const App = () => {
     return <Greeting name="Alice" />;
 };
+
+export default App; 
