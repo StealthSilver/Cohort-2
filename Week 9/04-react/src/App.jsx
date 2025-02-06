16cd7bf3c77d4ee17c8e65b50809fa1fccbaf00d
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-02-06 14:08:33 +0530

    react

--- Week 9/04-react/src/App.jsx
+++ Week 9/04-react/src/App.jsx
@@ -1,12 +1,21 @@
 function App() {
-  return <div>Hi there</div>;
+  return <div>
+    <PostComponent></PostComponent>
+  </div>;
 }
 
 
 function PostComponent(){
-  return <div style = {{width : 200 , backgroundColor : "white", borderRadius :10 , borderColor: "gray" , borderWidth: 1}}>
+  return <div style = {{width : 200 , backgroundColor : "white", borderRadius :10 , borderColor: "gray" , borderWidth: 1, display: "flex"}}>
 
-    <img src={"https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQwN0-cLq3vtAkGpSg6upZYLjRAbcu6ljrc5RZoXZkmoQObNT7szAUBgMCq8w6wMKDTNtvYb-4UFDGc470xr_BgCA"}></img>
+    <img src={"https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQwN0-cLq3vtAkGpSg6upZYLjRAbcu6ljrc5RZoXZkmoQObNT7szAUBgMCq8w6wMKDTNtvYb-4UFDGc470xr_BgCA"} style ={{
+      width: 25 , height: 25, borderRadius : 20
+    }}></img>
+
+    <div>
+      <b>GADHA</b>
+      <div>23,482 followers</div>
+    </div>
     </div>
 }
 
