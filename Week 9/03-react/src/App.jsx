cc1ffcee510dd91555af9b33ee32c9123988fd4e
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-02-06 13:21:17 +0530

    react

--- Week 9/03-react/src/App.jsx
+++ Week 9/03-react/src/App.jsx
@@ -3,53 +3,53 @@ import { useState, useEffect } from "react"
 
 
 function App() {
-  
 
-  const [count , setCount] = useState(0);
-  const [count2 , setCount2] = useState(0);
 
-  function increase(){
-    setCount(c => c +1)
+  const [count, setCount] = useState(0);
+  const [count2, setCount2] = useState(0);
+
+  function increase() {
+    setCount(c => c + 1)
   }
-  
-  function decrease(){
-    setCount2(c => c -1)
+
+  function decrease() {
+    setCount2(c => c - 1)
   }
-  
-  
+
+
 
   return (
     <div>
-      <Counter count = {count} count2 = {count2}/>
-      <button onClick = {increase}>Increase Count</button>
-      <button onClick = {decrease}>Decrease Count</button>
+      <Counter count={count} count2={count2} />
+      <button onClick={increase}>Increase Count</button>
+      <button onClick={decrease}>Decrease Count</button>
     </div>
   )
 
 
 }
 
-function Counter(props){
+function Counter(props) {
 
 
 
-  useEffect(function(){
+  useEffect(function () {
 
     console.log("mount")
 
-    return function(){
+    return function () {
       console.log("unmount")
     }
-    
+
   }, []);
 
-  useEffect(function(){
+  useEffect(function () {
     console.log("count has changed")
   }, [props.count])
 
 
   return <div>Counter 1 {props.count} <br />
-  Counter 2 {props.count2} </div>
+    Counter 2 {props.count2} </div>
 }
 
 export default App
