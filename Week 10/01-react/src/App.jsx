efef3be3e806cc23a2a4050fe3e1d16473f353b7
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-02-07 09:54:41 +0530

    react

--- Week 10/01-react/src/App.jsx
+++ Week 10/01-react/src/App.jsx
@@ -4,10 +4,21 @@ function App() {
   return (
     <div>
       <BrowserRouter>
-        <Routes></Routes>
+        <Routes>
+          <Route path = "/neet/online-coaching-class-11" element={<Class11Program/>}/>
+          <Route path = "/neet/online-coaching-class-12" element={<Class12Program/>}/>
+        </Routes>
       </BrowserRouter>
     </div>
   );
 }
+
+function Class11Program(){
+return <div>Class 11 NEET program</div>
+}
+
+function Class12Program(){
+  return <div>Class 12 NEET program</div>
+  }
 
 export default App;
