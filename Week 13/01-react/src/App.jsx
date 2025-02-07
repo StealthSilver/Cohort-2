45bc2c9fef0ed4c6892eda27b1e333aa1a60707a
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-02-08 00:43:59 +0530

    react

--- Week 13/01-react/src/App.jsx
+++ Week 13/01-react/src/App.jsx
@@ -1,5 +1,11 @@
 function App() {
-  return <div className="bg-blue-300">hello</div>;
+  return (
+    <div className="flex justify-between">
+      <div>child 1</div>
+      <div>child 2</div>
+      <div>child 3</div>
+    </div>
+  );
 }
 
 export default App;
