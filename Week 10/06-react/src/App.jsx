ead8e0b75167fc392c14f3ac10f7190c624599fa
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-02-07 11:34:58 +0530

    react

--- Week 10/06-react/src/App.jsx
+++ Week 10/06-react/src/App.jsx
@@ -1,7 +1,45 @@
-function App() {
+const CountContext = createContext();
+
+function CountContextProvider({ children }) {
   const [count, setCount] = useState(0);
 
-  return <></>;
+  return (
+    <CountContext.Provider value={{ count, setCount }}>
+      {children}
+    </CountContext.Provider>
+  );
 }
 
+function Parent() {
+  return (
+    <CountContextProvider>
+      <Incrase />
+      <Decrease />
+      <Value />
+    </CountContextProvider>
+  );
+}
+
+function Decrease() {
+  const { count, setCount } = useContext(CountContext);
+  return <button onClick={() => setCount(count - 1)}>Decrease</button>;
+}
+
+function Incrase() {
+  const { count, setCount } = useContext(CountContext);
+  return <button onClick={() => setCount(count + 1)}>Increase</button>;
+}
+
+function Value() {
+  const { count } = useContext(CountContext);
+  return <p>Count: {count}</p>;
+}
+
+const App = () => {
+  return (
+    <div>
+      <Parent />
+    </div>
+  );
+};
 export default App;
