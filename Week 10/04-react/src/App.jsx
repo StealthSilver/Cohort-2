cb67ca2977b92e7ab0c831a36c07ac9424c8823c
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-02-07 11:11:02 +0530

    react

--- Week 10/04-react/src/App.jsx
+++ Week 10/04-react/src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect, useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+function Parent() {
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Incrase count={count} setCount={setCount} />
+      <Decrease count={count} setCount={setCount} />
+      <Value count={count} setCount={setCount} />
     </>
-  )
+  );
 }
 
-export default App
+function Decrease({ count, setCount }) {
+  return <button onClick={() => setCount(count - 1)}>Decrease</button>;
+}
+
+function Incrase({ count, setCount }) {
+  return <button onClick={() => setCount(count + 1)}>Increase</button>;
+}
+
+function Value({ count }) {
+  return <p>Count: {count}</p>;
+}
+
+// App Component
+const App = () => {
+  return (
+    <div>
+      <Parent />
+    </div>
+  );
+};
+
+export default App;
