0ca09d1170223ab87fa1559b3608c22de9c0d2e1
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-02-08 00:51:05 +0530

    react

--- Week 13/02-react/src/App.jsx
+++ Week 13/02-react/src/App.jsx
@@ -1,9 +1,9 @@
 function App() {
   return (
-    <div className="flex justify-between">
-      <div>child 1</div>
-      <div>child 2</div>
-      <div>child 3</div>
+    <div className="grid grid-cols-12">
+      <div className="bg-blue-400 col-span-4">child 1</div>
+      <div className="bg-green-400 col-span-6">child 2</div>
+      <div className="bg-red-400 col-span-2">child 3</div>
     </div>
   );
 }
