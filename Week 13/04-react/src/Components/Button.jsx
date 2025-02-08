5260a23ffa3809c7591b2a1af19ae2a850fc88ba
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-02-08 10:55:07 +0530

    react

--- Week 13/04-react/src/Components/Button.jsx
+++ Week 13/04-react/src/Components/Button.jsx
@@ -1,10 +1,10 @@
 const Button = ({ children, disabled }) => {
   return (
     <button
-      className={`w-full p-2 rounded-lg font-semibold text-white transition duration-300 ${
+      className={`w-full p-2 rounded-lg font-semibold transition duration-300 ${
         disabled
-          ? "bg-gray-400 cursor-not-allowed"
-          : "bg-blue-600 hover:bg-blue-700"
+          ? "bg-gray-500 text-gray-300 cursor-not-allowed"
+          : "bg-blue-600 hover:bg-blue-700 text-white"
       }`}
       disabled={disabled}
     >
