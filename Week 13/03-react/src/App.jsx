a6a151fea686440db013253881dee54454cc1c81
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-02-08 10:34:03 +0530

    react

--- Week 13/03-react/src/App.jsx
+++ Week 13/03-react/src/App.jsx
@@ -1,6 +1,6 @@
 function App() {
   return (
-    <div className="grid grid-cols-3 sm:grid-cols-12">
+    <div className="grid grid-cols-1 sm:grid-cols-12">
       <div className="col-span-1 sm:col-span-5 bg-red-300">h1</div>
       <div className="col-span-1 sm:col-span-5 bg-green-300">h1</div>
       <div className="col-span-1 sm:col-span-2 bg-blue-300">h1</div>
