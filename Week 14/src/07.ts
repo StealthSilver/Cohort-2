c7018f021e56cb075774235e08372e6ba618284c
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-02-10 13:42:36 +0530

    1

--- Week 14/src/07.ts
+++ Week 14/src/07.ts
@@ -1,8 +1,10 @@
+//object as an argument
+
 function greetUser(user: { name: string; age: number }) {
     console.log("Hello " + user.name);
 }
 
-// Call the function with the correct name
+
 greetUser({
     name: "pogo",
     age: 87
