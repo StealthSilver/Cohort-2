719fa6991016bcc6deea42bbed64b45c954e50f2
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-02-10 13:07:27 +0530

    1

---
+++ Week 14/05.js
@@ -0,0 +1,5 @@
+"use strict";
+function nextSum(a, b) {
+    return a + b; // 'a' is a string, 'b' is a number, so the result is a string.
+}
+console.log(nextSum("3", 6)); // Output: "36"
---
+++ Week 14/05.ts
@@ -0,0 +1,5 @@
+function nextSum(a: string, b: number): string {
+    return a + b; // 'a' is a string, 'b' is a number, so the result is a string.
+}
+
+console.log(nextSum("3", 6)); // Output: "36"
