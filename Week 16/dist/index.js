db63225ca3e3718419e978731277dc7aa2801a59
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-02-11 10:25:05 +0530

    01

--- Week 16/dist/index.js
+++ Week 16/dist/index.js
@@ -1,13 +1,56 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || (function () {
+    var ownKeys = function(o) {
+        ownKeys = Object.getOwnPropertyNames || function (o) {
+            var ar = [];
+            for (var k in o) if (Object.prototype.hasOwnProperty.call(o, k)) ar[ar.length] = k;
+            return ar;
+        };
+        return ownKeys(o);
+    };
+    return function (mod) {
+        if (mod && mod.__esModule) return mod;
+        var result = {};
+        if (mod != null) for (var k = ownKeys(mod), i = 0; i < k.length; i++) if (k[i] !== "default") __createBinding(result, mod, k[i]);
+        __setModuleDefault(result, mod);
+        return result;
+    };
+})();
 Object.defineProperty(exports, "__esModule", { value: true });
-const ws_1 = require("ws");
+const ws_1 = __importStar(require("ws"));
 const wss = new ws_1.WebSocketServer({ port: 8080 });
 wss.on("connection", function (socket) {
-    console.log("user connected");
-    setInterval(() => {
-        socket.send("current price of solana is : " + Math.random());
+    console.log("User connected");
+    const interval = setInterval(() => {
+        if (socket.readyState === ws_1.default.OPEN) {
+            socket.send("Current price of Solana is: " + Math.random());
+        }
     }, 500);
-    socket.on("messgae", (e) => {
-        console.log(e.toString());
+    socket.on("message", (e) => {
+        console.log("Received message:", e.toString());
+    });
+    socket.on("close", () => {
+        console.log("User disconnected");
+        clearInterval(interval);
+    });
+    socket.on("error", (err) => {
+        console.error("WebSocket error:", err.message);
+        clearInterval(interval);
     });
 });
