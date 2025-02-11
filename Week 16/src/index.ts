ac2c59532c67b19c9099e0461d47e8e74b3df06f
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-02-11 10:19:23 +0530

    01

--- Week 16/src/index.ts
+++ Week 16/src/index.ts
@@ -1,1 +1,14 @@
-console.log("hi")
+import WebSocket, { WebSocketServer } from 'ws';
+
+const wss = new WebSocketServer({ port: 8080 });
+
+wss.on("connection" , function(socket){
+    console.log("user connected");
+    setInterval(() => {
+        socket.send("current price of solana is : " + Math.random());
+    },500);
+
+    socket.on("messgae" , (e)=> {
+        console.log(e.toString());
+    })
+})
