6a408934ba4233083f79d9c4ff46ed5019bd9b15
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-05-11 15:23:52 +0530

    init

---
+++ Week 21/paytm/backend/db.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+
+// create a user schemas
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+    minLength: 3,
+    maxLength: 30,
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: 6,
+  },
+  firstName: {
+    type: String,
+    required: true,
+    trim: true,
+    maxLength: 50,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true,
+    maxLength: 50,
+  },
+});
+
+// create a model from the schema
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = {
+  User,
+};
