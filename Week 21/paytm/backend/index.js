022d4c775a7ef16dbb1a96184b9c3bb7f4315fdb
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-05-11 15:52:21 +0530

    schemas

--- Week 21/paytm/backend/index.js
+++ Week 21/paytm/backend/index.js
@@ -1,8 +1,14 @@
 const express = require("express");
+const cors = require("cors");
 const mainRouter = require("./routes/index");
+
 const PORT = 8000;
 
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+
 app.use("/api/v1", mainRouter);
 
 app.listen(PORT, () => {
