53249184e6a3836300c132eac89aaa11a01e4b49
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-05-11 16:14:03 +0530

    schemas

--- Week 21/paytm/backend/routes/user.js
+++ Week 21/paytm/backend/routes/user.js
@@ -5,8 +5,9 @@ const zod = require("zod");
 const user = require("../db");
 const jwt = require("jsonwebtoken");
 const { JT_SECRET } = require("../config");
+const JWT_SECRET = require("../config");
 
-// signin and signup routes
+// signin
 
 const signupBody = zod.object({
   username: zod.string().email(),
@@ -19,9 +20,42 @@ router.post("/signup", async (req, res) => {
   const { success } = signupBody.safeParse(req.body);
   if (!success) {
     return req.status(411).json({
-      message: "Email already taken / wrong inputs",
+      message: "wrong inputs",
     });
   }
+
+  const existingUser = await user.User.findOne({
+    username: req.body.username,
+  });
+
+  if (existingUser) {
+    return res.status(411).json({
+      message: "Email already exists",
+    });
+  }
+
+  const user = await User.create({
+    username: req.body.username,
+    password: req.body.password,
+    firstName: req.bosy.firstName,
+    lastName: req.body.lastName,
+  });
+
+  const userId = user._id;
+
+  const token = jwt.sign(
+    {
+      userId,
+    },
+    JWT_SECRET
+  );
+
+  res.json({
+    message: "User created successfully",
+    token: token,
+  });
+
+  // signup
 });
 
 module.exports = router;
