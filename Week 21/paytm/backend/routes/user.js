a1658bb4c7aaabd2f3baa2286bc7ab7581e1f3d9
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-05-11 16:08:22 +0530

    schemas

--- Week 21/paytm/backend/routes/user.js
+++ Week 21/paytm/backend/routes/user.js
@@ -1,4 +1,27 @@
 const express = require("express");
 const router = express.Router();
 
+const zod = require("zod");
+const user = require("../db");
+const jwt = require("jsonwebtoken");
+const { JT_SECRET } = require("../config");
+
+// signin and signup routes
+
+const signupBody = zod.object({
+  username: zod.string().email(),
+  password: zod.string(),
+  firstName: zod.string(),
+  lastName: zod.string(),
+});
+
+router.post("/signup", async (req, res) => {
+  const { success } = signupBody.safeParse(req.body);
+  if (!success) {
+    return req.status(411).json({
+      message: "Email already taken / wrong inputs",
+    });
+  }
+});
+
 module.exports = router;
