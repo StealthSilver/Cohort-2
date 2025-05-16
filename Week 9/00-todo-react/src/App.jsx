4eaa0aac5f748ea3b5450eedc591de37d5f625ae
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-05-17 02:34:45 +0530

    frontend

--- Week 9/00-todo-react/src/App.jsx
+++ Week 9/00-todo-react/src/App.jsx
@@ -3,7 +3,26 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [todos, setTodos] = useState([
+    {
+      title: "go to gym",
+      description: "regularly",
+      done: false,
+    },
+  ]);
+
+  function addTodo() {
+    let newArray = [];
+    for (let i = 0; i < todos.length; i++) {
+      newArray.push(todos[i]);
+    }
+
+    newArray.push({
+      title: "eat food",
+      description: "properly",
+      done: true,
+    });
+  }
 
   return <div></div>;
 }
