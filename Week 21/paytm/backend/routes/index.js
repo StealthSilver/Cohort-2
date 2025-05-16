99b161f0385e032262bbad78309ba7f00acbb27c
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-05-16 22:46:46 +0530

    first

--- Week 21/paytm/backend/routes/index.js
+++ Week 21/paytm/backend/routes/index.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const router = express.Router();
+
 const userRouter = require("./user");
 const accountRouter = require("./account");
 
 // all requests will start as /api/v1/user
+const router = express.Router();
 
 router.use("/user", userRouter);
 router.use("/account", accountRouter);
