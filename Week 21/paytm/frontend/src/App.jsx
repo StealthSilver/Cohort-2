5d59086af08e4b4b1dc7ab6bb33c6cd6677d966c
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-05-16 23:28:12 +0530

    frontend

--- Week 21/paytm/frontend/src/App.jsx
+++ Week 21/paytm/frontend/src/App.jsx
@@ -1,12 +1,23 @@
-import React, { useState, useEffect } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import { Signup } from "./pages/Signup";
+import { Signin } from "./pages/Signin";
+import { Dashboard } from "./pages/Dashboard";
+import { SendMoney } from "./pages/SendMoney";
 
-function Timer() {
-  const [seconds, setSeconds] = useState(0);
-
-  useEffect(() => {
-    const interval = setInterval(() => setSeconds((s) => s + 1), 1000);
-    return () => clearInterval(interval); // Cleanup
-  }, []);
-
-  return <p>Timer: {seconds} seconds</p>;
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/send" element={<SendMoney />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
+
+export default App;
