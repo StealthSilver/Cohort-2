7f639385994850ac83fc30527c4b1ba117fae976
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-05-16 19:33:18 +0530

    first

--- Week 21/paytm/frontend/src/App.jsx
+++ Week 21/paytm/frontend/src/App.jsx
@@ -1,5 +1,12 @@
-function App() {
-  return <div>Hello World</div>;
-}
+import React, { useState, useEffect } from "react";
 
-export default App;
+function Timer() {
+  const [seconds, setSeconds] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => setSeconds((s) => s + 1), 1000);
+    return () => clearInterval(interval); // Cleanup
+  }, []);
+
+  return <p>Timer: {seconds} seconds</p>;
+}
