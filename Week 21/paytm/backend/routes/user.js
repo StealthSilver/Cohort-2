573fc40dcfd4ecd2046868dc27f63f39dd8e0597
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-05-16 22:46:11 +0530

    first

--- Week 21/paytm/backend/routes/user.js
+++ Week 21/paytm/backend/routes/user.js
@@ -1,49 +1,50 @@
+// backend/routes/user.js
 const express = require("express");
-const router = express.Router();
 
+const router = express.Router();
 const zod = require("zod");
 const { User, Account } = require("../db");
 const jwt = require("jsonwebtoken");
 const { JWT_SECRET } = require("../config");
-const JWT_SECRET = require("../config");
 const { authMiddleware } = require("../middleware");
-
-// signin
 
 const signupBody = zod.object({
   username: zod.string().email(),
-  password: zod.string(),
   firstName: zod.string(),
   lastName: zod.string(),
+  password: zod.string(),
 });
 
 router.post("/signup", async (req, res) => {
   const { success } = signupBody.safeParse(req.body);
   if (!success) {
-    return req.status(411).json({
-      message: "wrong inputs",
+    return res.status(411).json({
+      message: "Email already taken / Incorrect inputs",
     });
   }
 
-  const existingUser = await user.User.findOne({
+  const existingUser = await User.findOne({
     username: req.body.username,
   });
 
   if (existingUser) {
     return res.status(411).json({
-      message: "Email already exists",
+      message: "Email already taken/Incorrect inputs",
     });
   }
 
-  // todo -> add bcrypt to hash and salt the passwords
   const user = await User.create({
     username: req.body.username,
     password: req.body.password,
-    firstName: req.bosy.firstName,
+    firstName: req.body.firstName,
     lastName: req.body.lastName,
   });
-
   const userId = user._id;
+
+  await Account.create({
+    userId,
+    balance: 1 + Math.random() * 10000,
+  });
 
   const token = jwt.sign(
     {
@@ -56,49 +57,44 @@ router.post("/signup", async (req, res) => {
     message: "User created successfully",
     token: token,
   });
-
-  // signup
-
-  const signinBody = zod.object({
-    username: zod.string().email(),
-    password: zod.string(),
-  });
-
-  router.post("/sgnin", async (req, res) => {
-    const { success } = signinBody.safeParse(req.body);
-
-    if (!success) {
-      return res.status(411).json({
-        message: "Email already taken / Incorrect inputs",
-      });
-    }
-
-    const user = await user.User.findOne({
-      username: req.body.username,
-      password: req.body.password,
-    });
-
-    if (user) {
-      const token = jwt.sign(
-        {
-          userId: user._id,
-        },
-        JWT_SECRET
-      );
-
-      res.json({
-        token: token,
-      });
-      return;
-    }
-
-    res.status(411).json({
-      message: "error while signing in",
-    });
-  });
 });
 
-// updating the password, names
+const signinBody = zod.object({
+  username: zod.string().email(),
+  password: zod.string(),
+});
+
+router.post("/signin", async (req, res) => {
+  const { success } = signinBody.safeParse(req.body);
+  if (!success) {
+    return res.status(411).json({
+      message: "Email already taken / Incorrect inputs",
+    });
+  }
+
+  const user = await User.findOne({
+    username: req.body.username,
+    password: req.body.password,
+  });
+
+  if (user) {
+    const token = jwt.sign(
+      {
+        userId: user._id,
+      },
+      JWT_SECRET
+    );
+
+    res.json({
+      token: token,
+    });
+    return;
+  }
+
+  res.status(411).json({
+    message: "Error while logging in",
+  });
+});
 
 const updateBody = zod.object({
   password: zod.string().optional(),
@@ -122,8 +118,6 @@ router.put("/", authMiddleware, async (req, res) => {
     message: "Updated successfully",
   });
 });
-
-// searching the name
 
 router.get("/bulk", async (req, res) => {
   const filter = req.query.filter || "";
