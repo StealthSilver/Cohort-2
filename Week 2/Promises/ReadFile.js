0fbba8e26cd3589ab4c1b363b7a84f349e41a7dd
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-06-17 23:38:11 +0530

    p.then

--- Week 2/Promises/ReadFile.js
+++ Week 2/Promises/ReadFile.js
@@ -5,7 +5,6 @@ const fs = require("fs/promises"); // Import fs module with promises
 async function readFileAsync() {
   try {
     console.log("Reading file...");
-
     let data = await fs.readFile("notes.txt", "utf-8"); // Wait for file to be read
     console.log("File content:", data);
   } catch (error) {
