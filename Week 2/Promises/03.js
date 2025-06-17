e236c4d0e2b5d6e26b8e08c10b159f1eeefde49f
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-06-17 23:13:14 +0530

    promise chaining

--- Week 2/Promises/03.js
+++ Week 2/Promises/03.js
@@ -3,4 +3,5 @@ function callback() {
 }
 
 setTimeout(callback, 3000);
-// the set timeout promises to call the callback function after 3 seconds
+// the set timeout promises to call the callback function after 3 seconds, this is the callback based approach
+// we an use the promise based approach
