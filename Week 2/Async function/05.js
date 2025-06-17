45dcffceb8f5c98966047eabca17c87b3e4b72b4
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-06-17 13:05:28 +0530

    async files

--- Week 2/Async function/05.js
+++ Week 2/Async function/05.js
@@ -2,6 +2,7 @@
 
 console.log("Hi!");
 
+// timeout is the callback function called back by setTimeout after 5000 ms
 setTimeout(function timeout() {
   console.log("set tiumeout complete");
 }, 5000);
