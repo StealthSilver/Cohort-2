0a930b78b8bd08470d392e4c893a06c61bb1efa5
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-06-17 12:39:40 +0530

    async files

--- Week 2/Async function/02.js
+++ Week 2/Async function/02.js
@@ -3,5 +3,12 @@ const fs = require("fs");
 const contents = fs.readFileSync("a.txt", "utf-8"); // synchronous reading
 console.log(contents);
 
-const contents2 = fs.readFile("b.txt", "utf-8"); // asynchronous reading
+const contents2 = fs.readFile("b.txt", "utf-8", (err, data) => {
+  if (err) {
+    console.error("Error reading b.txt:", err);
+  } else {
+    console.log("Async content:", data);
+  }
+}); // asynchronous reading
+
 console.log(contents2);
