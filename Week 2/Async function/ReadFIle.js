b3580d151edccd849009e0474b102f6457edc652
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-06-17 13:01:48 +0530

    async files

--- Week 2/Async function/ReadFIle.js
+++ Week 2/Async function/ReadFIle.js
@@ -5,7 +5,7 @@ const contents = fs.readFileSync("a.txt", "utf-8");
 console.log(contents);
 
 // Asynchronous reading with a callback
-// the third function is called back after the readFile completes
+// the callback function is called back after the readFile completes
 fs.readFile("b.txt", "utf-8", (err, contents2) => {
   if (err) {
     console.error("Error reading b.txt:", err);
