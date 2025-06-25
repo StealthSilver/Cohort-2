8234695e7ba8c4431aac13a035ad0f6564809f5d
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-06-25 13:27:04 +0530

    git logs

--- Week 5/03-http-server/index.js
+++ Week 5/03-http-server/index.js
@@ -3,6 +3,8 @@
 const express = require("express");
 const app = express();
 
+const PORT = 3000;
+
 app.get("/sum", function (req, res) {
   const a = parseInt(req.query.a);
   const b = parseInt(req.query.b);
@@ -39,4 +41,6 @@ app.get("/div", function (req, res) {
   });
 });
 
-app.listen(3000);
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
