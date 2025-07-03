d0ef8e428bc3ff459f3215529532e10d32e51ee8
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-07-03 21:29:14 +0530

    localhost

--- Week 4/03-httpServer/index.js
+++ Week 4/03-httpServer/index.js
@@ -5,6 +5,20 @@ const PORT = 3000;
 
 // in memory database
 
+const users = [
+  {
+    name: "Jhon",
+    kidneys: [
+      {
+        healthy: false,
+      },
+      {
+        healthy: true,
+      },
+    ],
+  },
+];
+
 app.listen(3000, () => {
   console.log(`app is running on port ${PORT}`);
 });
