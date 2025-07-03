d5bdfea2826a742aae2ef9f4c70d095e47ae925e
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-07-03 21:47:39 +0530

    localhost

--- Week 4/03-httpServer/index.js
+++ Week 4/03-httpServer/index.js
@@ -61,6 +61,7 @@ app.put("/", function (req, res) {
   for (let i = 0; i < users[0].kidneys.length; i++) {
     users[0].kidneys[i].healthy = true;
   }
+  res.json({});
 });
 
 app.listen(3000, () => {
