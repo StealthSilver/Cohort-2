2a2b52eaa9c7d93f9bb3fc6c15f2f5a5590f52ce
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-07-03 21:32:25 +0530

    localhost

--- Week 4/03-httpServer/index.js
+++ Week 4/03-httpServer/index.js
@@ -19,6 +19,20 @@ const users = [
   },
 ];
 
+app.get("/", function (req, res) {
+  const johnKidneys = users[0].kidneys;
+
+  const numberOfKidneys = kidneys.length;
+
+  let numberOfHealthyKidneys = 0;
+
+  for (let i = 0; i < johnKidneys.length; i++) {
+    if (johnKidneys[i].healthy) {
+      numberOfHealthyKidneys = numberOfHealthyKidneys + 1;
+    }
+  }
+});
+
 app.listen(3000, () => {
   console.log(`app is running on port ${PORT}`);
 });
