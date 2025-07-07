4931c200ad23e1e99c2b3c88818457ef4aa4b106
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-07-07 12:18:51 +0530

    dev mode

--- Week 7/01-mongo/auth.js
+++ Week 7/01-mongo/auth.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 const JWT_SECRET = "s3cret";
 
 function auth(req, res, next) {
-  const token = req.headers.authorization;
+  const token = req.headers.token;
 
   if (!token) {
     return res.status(401).json({ message: "No token provided." });
