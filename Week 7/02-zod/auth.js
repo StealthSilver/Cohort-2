af660c61163e3c4b56f65c3f5dc77ea7d4e1d980
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-07-07 12:50:27 +0530

    dev mode

--- Week 7/02-zod/auth.js
+++ Week 7/02-zod/auth.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 const JWT_SECRET = "s3cret";
 
 function auth(req, res, next) {
-  const token = req.headers.authorization;
+  const token = req.headers.token;
 
   const response = jwt.verify(token, JWT_SECRET);
 
