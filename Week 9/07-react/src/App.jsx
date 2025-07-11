2d614222897a2feec712dbb4b36e4d4cef02d187
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-07-11 20:34:59 +0530

    dev mode

--- Week 9/07-react/src/App.jsx
+++ Week 9/07-react/src/App.jsx
@@ -1,24 +1,23 @@
-import {useEffect , useState} from "react";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(1)
+  const [count, setCount] = useState(1);
 
-function increaseCount(){
-  setCount (currentValue => currentValue + 1)
-}
+  function increaseCount() {
+    setCount((currentValue) => currentValue + 1);
+  }
 
-// this code will only run on mount and not on every render
-useEffect(()=> {
-  console.log("above setinterval")
-  setInterval(increaseCount, 1000);
-}, [])
-
+  // this code will only run on mount and not on every render
+  useEffect(() => {
+    console.log("above setinterval");
+    setInterval(increaseCount, 1000);
+  }, []);
 
   return (
     <div>
-      {count}
+      <h1> {count}</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
