d373338bdad7aeb268298a638e6787fbe07dcb91
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-07-11 20:37:44 +0530

    dev mode

--- Week 9/08-react/src/App.jsx
+++ Week 9/08-react/src/App.jsx
@@ -1,19 +1,19 @@
-import {useEffect , useState} from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   const [count, setCount] = useState(1);
   const [count2, setCount2] = useState(1);
 
   function increaseCount() {
-    setCount(currentValue => currentValue + 1);
+    setCount((currentValue) => currentValue + 1);
   }
 
   function decreaseCount() {
-    setCount2(currentValue => currentValue - 1);
+    setCount2((currentValue) => currentValue - 1);
   }
 
   // this code will only run on mount and not on every render
-  useEffect(()=> {
+  useEffect(() => {
     const increaseInterval = setInterval(increaseCount, 3000);
     const decreaseInterval = setInterval(decreaseCount, 4000);
 
@@ -23,12 +23,12 @@ function App() {
       clearInterval(decreaseInterval);
       console.log("Intervals cleared on unmount.");
     };
-  }, []);  
+  }, []);
   // this effect will run only on mount, because the array is empty
 
-  useEffect(()=> {
+  useEffect(() => {
     console.log("the count has been updated to " + count);
-  }, [count ,count2]);  
+  }, [count, count2]);
   // this effect will run every time the count2 changes as there is the variable in the dependency array
 
   return (
