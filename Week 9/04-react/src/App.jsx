0452c97fa283d5561b725eb8508c1affc69be0a9
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-07-11 19:38:28 +0530

    dev mode

--- Week 9/04-react/src/App.jsx
+++ Week 9/04-react/src/App.jsx
@@ -1,57 +1,60 @@
 function App() {
   return (
-    <div style = {{backgroundColor: "#dfe6e9" , height: "100vh"}}>
-      <div >
-        <div style={{display: "flex" , justifyContent: "center"}}><PostComponent></PostComponent></div>
-      
-      <br />
-      <div style={{display: "flex" , justifyContent: "center"}}><PostComponent></PostComponent></div>
-      <br />
-      <div style={{display: "flex" , justifyContent: "center"}}><PostComponent></PostComponent></div>
+    <div style={{ backgroundColor: "#dfe6e9", height: "100vh" }}>
+      <div>
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <PostComponent></PostComponent>
+        </div>
+        <br />
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <PostComponent></PostComponent>
+        </div>
+        <br />
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <PostComponent></PostComponent>
+        </div>
       </div>
     </div>
   );
 }
 
 function PostComponent() {
-  return(
-
-  <div  style={{
-    width: 200,
-    backgroundColor: "white",
-    borderRadius: 10,
-    borderColor: "gray",
-    borderWidth: 1,
-    padding: 20
-   
-    
-  }}>
-    <div style = {{display: "flex"}}
-     
+  return (
+    <div
+      style={{
+        width: 200,
+        backgroundColor: "white",
+        borderRadius: 10,
+        borderColor: "gray",
+        borderWidth: 1,
+        padding: 20,
+      }}
     >
-      <img
-        src={
-          "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQwN0-cLq3vtAkGpSg6upZYLjRAbcu6ljrc5RZoXZkmoQObNT7szAUBgMCq8w6wMKDTNtvYb-4UFDGc470xr_BgCA"
-        }
-        style={{
-          width: 35,
-          height: 35,
-          borderRadius: 20,
-        }}
-      ></img>
+      <div style={{ display: "flex" }}>
+        <img
+          src={
+            "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQwN0-cLq3vtAkGpSg6upZYLjRAbcu6ljrc5RZoXZkmoQObNT7szAUBgMCq8w6wMKDTNtvYb-4UFDGc470xr_BgCA"
+          }
+          style={{
+            width: 35,
+            height: 35,
+            borderRadius: 20,
+          }}
+        ></img>
 
-      <div style = {{fontSize:14 , marginLeft: 10}}>
-        <b>GADHA</b>
-        <div>23,482 followers</div>
-        <div>12m</div>
+        <div style={{ fontSize: 14, marginLeft: 10 }}>
+          <b>GADHA</b>
+          <div>23,482 followers</div>
+          <div>12m</div>
+        </div>
+      </div>
+
+      <div>
+        Want to know how to win big? check out how these folks won $6000 in
+        bounties
       </div>
     </div>
-
-    <div>
-      Want to know how to win big? check out how these folks won $6000 in
-      bounties
-    </div>
-  </div>)
+  );
 }
 
 export default App;
