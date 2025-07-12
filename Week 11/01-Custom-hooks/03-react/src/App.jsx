220387a61c07b21158590891782b682e5dbc2339
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-07-12 12:56:44 +0530

    dev mode

--- Week 11/01-Custom-hooks/03-react/src/App.jsx
+++ Week 11/01-Custom-hooks/03-react/src/App.jsx
@@ -1,22 +1,10 @@
 import { useState, useEffect } from "react";
 
 import "./App.css";
+import { usePostTitle } from "./hooks/useFetch";
 
 function App() {
-  const [post, setPost] = useState({});
-
-  async function getPosts() {
-    const response = await fetch(
-      "https://jsonplaceholder.typicode.com/posts/3"
-    );
-    const json = await response.json();
-    setPost(json);
-  }
-
-  // getPost is written in the useEffect as if the dependency array is empty the function written inside the useEffect is called as soon as the App component mounts
-  useEffect(() => {
-    getPosts();
-  }, []);
+  const postTitle = usePostTitle;
 
   return (
     <>
