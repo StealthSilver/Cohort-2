c8db40c99764ef88170280dc6723e0a8df6bf80f
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-07-12 13:06:55 +0530

    dev mode

--- Week 11/01-Custom-hooks/04-react/src/App.jsx
+++ Week 11/01-Custom-hooks/04-react/src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from "react";
 
 import "./App.css";
+import { useFetch } from "./hooks/useFetch";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const { finalData } = useFetch(
+    "https://jsonplaceholder.typicode.com/posts/3"
+  );
 
-  return <></>;
+  return <div>{JSON.stringify(finalData)}</div>;
 }
 
 export default App;
