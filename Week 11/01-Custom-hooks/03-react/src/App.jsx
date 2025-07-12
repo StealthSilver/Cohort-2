a42b0787757889c9669418c0a2c6985c9e96e8a5
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-07-12 12:54:43 +0530

    dev mode

--- Week 11/01-Custom-hooks/03-react/src/App.jsx
+++ Week 11/01-Custom-hooks/03-react/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
 
   async function getPosts() {
     const response = await fetch(
-      "https://jsonplaceholder.typicode.com/posts/1"
+      "https://jsonplaceholder.typicode.com/posts/3"
     );
     const json = await response.json();
     setPost(json);
