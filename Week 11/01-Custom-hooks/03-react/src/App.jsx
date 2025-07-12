dde3f3bb336e20b80198c40c5900a62bf05674bc
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-07-12 12:53:48 +0530

    dev mode

--- Week 11/01-Custom-hooks/03-react/src/App.jsx
+++ Week 11/01-Custom-hooks/03-react/src/App.jsx
@@ -12,6 +12,7 @@ function App() {
     const json = await response.json();
     setPost(json);
 
+    // getPost is written in the useEffect as if the dependency array is empty the function written inside the useEffect is called as soon as the App component mounts
     useEffect(() => {
       getPosts();
     }, []);
@@ -19,7 +20,7 @@ function App() {
 
   return (
     <>
-      <div>hi there</div>
+      <div>{post.title}</div>
     </>
   );
 }
