d088940e6970586ea2983c5533321d90132119bc
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-07-12 12:51:47 +0530

    dev mode

--- Week 11/01-Custom-hooks/03-react/src/App.jsx
+++ Week 11/01-Custom-hooks/03-react/src/App.jsx
@@ -1,11 +1,27 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [post, setPost] = useState({});
 
-  return <></>;
+  async function getPosts() {
+    const response = await fetch(
+      "https://jsonplaceholder.typicode.com/posts/1"
+    );
+    const json = await response.json();
+    setPost(json);
+
+    useEffect(() => {
+      getPosts();
+    }, []);
+  }
+
+  return (
+    <>
+      <div>hi there</div>
+    </>
+  );
 }
 
 export default App;
