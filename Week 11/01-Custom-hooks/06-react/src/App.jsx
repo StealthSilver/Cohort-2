a4bf7b0d124dcd95ef43d555c13f384776a5619f
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-07-12 13:35:28 +0530

    trees

--- Week 11/01-Custom-hooks/06-react/src/App.jsx
+++ Week 11/01-Custom-hooks/06-react/src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { usePrev } from "./hooks/usePrev";
 import "./App.css";
 
 function App() {
