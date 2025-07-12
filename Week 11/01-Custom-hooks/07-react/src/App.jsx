e0809b6f75828869dd4eb9c515facf408b621243
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-07-12 14:56:47 +0530

    trees

--- Week 11/01-Custom-hooks/07-react/src/App.jsx
+++ Week 11/01-Custom-hooks/07-react/src/App.jsx
@@ -1,6 +1,13 @@
 import { useState } from "react";
 
-import "./App.css";
+function useDebounce(originalFn) {
+  const currentClock = useRef();
+
+  const fn = () => {
+    clearTimeout(currentClock.current);
+    currentClock.current = setTimeout(originalFn, 200);
+  };
+}
 
 function App() {
   function sendDataToBackend() {
