b804a7af6926d6d9fb3534274892f1fa6f88a458
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-07-14 02:57:14 +0530

    trees

--- Week 13/05-react/src/App.jsx
+++ Week 13/05-react/src/App.jsx
@@ -1,8 +1,7 @@
-
-import { useEffect, useState } from 'react'
-import './App.css'
-import { Sidebar4 } from './components/sidebars/Sidebar4'
-import { SidebarToggle } from './components/icons/SidebarToggle';
+import { useEffect, useState } from "react";
+import "./App.css";
+import { Sidebar4 } from "./components/sidebars/Sidebar4";
+import { SidebarToggle } from "./components/icons/SidebarToggle";
 
 const useMediaQuery = (query) => {
   const [matches, setMatches] = useState(false);
@@ -20,66 +19,70 @@ const useMediaQuery = (query) => {
   return matches;
 };
 
-
 function App() {
-
-  const [sidebarOpen, setSidebarOpen] =  useState(true);
+  const [sidebarOpen, setSidebarOpen] = useState(true);
   const isDesktop = useMediaQuery("(min-width: 768px)");
 
-  console.error(isDesktop)
-  
+  console.error(isDesktop);
+
   useEffect(() => {
     if (isDesktop == false) {
-      setSidebarOpen(false)
+      setSidebarOpen(false);
     } else {
-      setSidebarOpen(true)
+      setSidebarOpen(true);
     }
-  }, [isDesktop])
+  }, [isDesktop]);
 
   return (
-    <div className='flex'>
+    <div className="flex">
       <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
       <MainContent sidebarOpen={sidebarOpen} />
     </div>
-  )
+  );
 }
 
-function Sidebar({sidebarOpen, setSidebarOpen}) {
+function Sidebar({ sidebarOpen, setSidebarOpen }) {
   if (!sidebarOpen) {
-    return <div className='fixed top-0 left-0'>
-        <div className='cursor-pointer hover:bg-slate-200' onClick={() => {
-          setSidebarOpen(!sidebarOpen)
-        }}>
+    return (
+      <div className="fixed top-0 left-0">
+        <div
+          className="cursor-pointer hover:bg-slate-200"
+          onClick={() => {
+            setSidebarOpen(!sidebarOpen);
+          }}
+        >
           <SidebarToggle />
         </div>
-    </div>
+      </div>
+    );
   }
-    return <div className='w-96 h-screen bg-red-100 fixed top-0 left-0 md:relative'>
-    <div>
-      <div className='cursor-pointer hover:bg-slate-200' onClick={() => {
-        setSidebarOpen(!sidebarOpen)
-      }}>
-        <SidebarToggle />
+  return (
+    <div className="w-96 h-screen bg-red-100 fixed top-0 left-0 md:relative">
+      <div>
+        <div
+          className="cursor-pointer hover:bg-slate-200"
+          onClick={() => {
+            setSidebarOpen(!sidebarOpen);
+          }}
+        >
+          <SidebarToggle />
+        </div>
       </div>
     </div>
-  </div>
+  );
 }
 
 function MainContent() {
-  return  <div className='w-full'>
-    <div className='h-72 bg-black hidden md:block'></div>
-    <div className='grid grid-cols-11 gap-8 p-8'>
-      <div className='h-96 rounded-2xl shadow bg-red-200 md:col-span-2 -translate-y-24 shadow-lg  col-span-11 hidden md:block'>
-
-      </div>
-      <div className='h-96 rounded-2xl shadow bg-green-200 md:col-span-6 shadow-lg col-span-11'>
-
-      </div>
-      <div className='h-96 rounded-2xl shadow bg-yellow-200 md:col-span-3 shadow-lg col-span-11'>
-
+  return (
+    <div className="w-full">
+      <div className="h-72 bg-black hidden md:block"></div>
+      <div className="grid grid-cols-11 gap-8 p-8">
+        <div className="h-96 rounded-2xl shadow bg-red-400 md:col-span-2 -translate-y-24 shadow-lg  col-span-11 hidden md:block"></div>
+        <div className="h-96 rounded-2xl shadow bg-green-200 md:col-span-6 shadow-lg col-span-11"></div>
+        <div className="h-96 rounded-2xl shadow bg-yellow-200 md:col-span-3 shadow-lg col-span-11"></div>
       </div>
     </div>
-  </div>
+  );
 }
 
-export default App
+export default App;
