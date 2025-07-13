d0f8e921137672b683ce0b1d091eb241fb95c130
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-07-14 02:44:15 +0530

    trees

--- Week 13/02-react/src/App.jsx
+++ Week 13/02-react/src/App.jsx
@@ -1,6 +1,6 @@
 function App() {
   return (
-    <div className="grid grid-cols-12">
+    <div className="grid grid-cols-12 bg-black">
       <div className="bg-black col-span-4">child 1</div>
       <div className="bg-green-400 col-span-6">child 2</div>
       <div className="bg-red-400 col-span-2">child 3</div>
