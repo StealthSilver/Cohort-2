766f6fb1dc047c9ec823e373506df3c0aad96499
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-07-14 12:28:21 +0530

    recoil

--- Week 11/02-Recoil/01-react/src/App.jsx
+++ Week 11/02-Recoil/01-react/src/App.jsx
@@ -1,12 +1,23 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return <></>;
+}
+
+function Increase() {
+  return (
+    <div>
+      <button>Increase</button>
+    </div>
+  );
+}
+
+function Decrease() {
+  return (
+    <div>
+      <button>Decrease</button>
+    </div>
+  );
 }
 
 export default App;
