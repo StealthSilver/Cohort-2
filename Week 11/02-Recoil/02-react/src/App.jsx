56a48dc52ecbcf8b8ef173d2724b0298865e682f
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-07-14 12:42:20 +0530

    recoil

--- Week 11/02-Recoil/02-react/src/App.jsx
+++ Week 11/02-Recoil/02-react/src/App.jsx
@@ -1,9 +1,20 @@
 import { useState } from "react";
-
+import { RecoilRoot } from "recoil";
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <RecoilRoot>
+      <Counter></Counter>
+    </RecoilRoot>
+  );
+}
+
+function Counter() {
+  const counter = atom({
+    key: "counter",
+    default: 0,
+  });
 }
 
 export default App;
