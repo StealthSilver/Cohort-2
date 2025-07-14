ae8d5131651edcbe7833f948353840c26be28ab3
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-07-14 13:16:05 +0530

    recoil

--- Week 11/02-Recoil/04-react/src/store/atoms/counter.js
+++ Week 11/02-Recoil/04-react/src/store/atoms/counter.js
@@ -10,7 +10,8 @@ export const counterAtom = atom({
 export const evenSelector = selector({
   key: "isEvenSelector",
   get: function ({ get }) {
+    const currentCount = get(counterAtom);
     const isEven = currentCount % 2 == 0;
-    return;
+    return isEven;
   },
 });
