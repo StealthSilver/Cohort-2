9e863cfbd0c7eacb08be93b5c343c95cb92ebe5e
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-07-15 12:29:01 +0530

    recoil

--- Week 11/02-Recoil/06-react/src/App.jsx
+++ Week 11/02-Recoil/06-react/src/App.jsx
@@ -7,18 +7,19 @@ import {
   notificationsAtom,
 } from "./model/atoms/atoms";
 
-import "./App.css";
+import { totalNotificationSelector } from "./model/atoms/atoms";
 
+import "./App.css";
 function App() {
   const networkNotificationCount = useRecoilValue(networkAtom);
-
   const notificationCount = useRecoilValue(notificationsAtom);
   const jobsCount = useRecoilValue(jobsAtom);
   const [messageCount, setMessageCount] = useRecoilState(messagingAtom);
+  const totalNotificationCount = useRecoilValue(totalNotificationSelector);
+
   return (
     <>
       <button>Home</button>
-
       <button>
         My Network (
         {networkNotificationCount >= 100 ? "99+" : networkNotificationCount})
@@ -27,14 +28,11 @@ function App() {
       <button>Messaging ({messageCount})</button>
       <button>Notification ({notificationCount})</button>
 
-      {/* increase the messaging count -> will be done by a backend */}
-      <button
-        onClick={() => {
-          setMessageCount(messageCount + 1);
-        }}
-      >
-        Me
-      </button>
+      {/* Increment messaging count */}
+      <button onClick={() => setMessageCount(messageCount + 1)}>Me</button>
+
+      {/* Now this shows separately */}
+      <button>Total: {totalNotificationCount}</button>
     </>
   );
 }
