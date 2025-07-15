144bba859f87deab9d87a60203033fc282c9ae85
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-07-15 12:09:27 +0530

    recoil

--- Week 11/02-Recoil/05-react/src/App.jsx
+++ Week 11/02-Recoil/05-react/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useRecoilValue } from "recoil";
+import { useRecoilValue, useRecoilState } from "recoil";
 import {
   jobsAtom,
   messagingAtom,
@@ -11,10 +11,10 @@ import "./App.css";
 
 function App() {
   const networkNotificationCount = useRecoilValue(networkAtom);
-  const messageCount = useRecoilValue(messagingAtom);
+
   const notificationCount = useRecoilValue(notificationsAtom);
   const jobsCount = useRecoilValue(jobsAtom);
-
+  const [messageCount, setMessageCount] = useRecoilState(messagingAtom);
   return (
     <>
       <button>Home</button>
@@ -26,7 +26,15 @@ function App() {
       <button>Jobs ({jobsCount})</button>
       <button>Messaging ({messageCount})</button>
       <button>Notification ({notificationCount})</button>
-      <button>Me</button>
+
+      {/* increase the messaging count */}
+      <button
+        onClick={() => {
+          setMessageCount(messageCount + 1);
+        }}
+      >
+        Me
+      </button>
     </>
   );
 }
