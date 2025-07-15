97f2795120656ae3463b7f2ae81f294a1eed81a5
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-07-15 12:07:11 +0530

    recoil

--- Week 11/02-Recoil/05-react/src/App.jsx
+++ Week 11/02-Recoil/05-react/src/App.jsx
@@ -1,11 +1,19 @@
 import { useState } from "react";
 import { useRecoilValue } from "recoil";
-import { networkAtom } from "./model/atoms/atoms";
+import {
+  jobsAtom,
+  messagingAtom,
+  networkAtom,
+  notificationsAtom,
+} from "./model/atoms/atoms";
 
 import "./App.css";
 
 function App() {
   const networkNotificationCount = useRecoilValue(networkAtom);
+  const messageCount = useRecoilValue(messagingAtom);
+  const notificationCount = useRecoilValue(notificationsAtom);
+  const jobsCount = useRecoilValue(jobsAtom);
 
   return (
     <>
@@ -15,9 +23,9 @@ function App() {
         My Network (
         {networkNotificationCount >= 100 ? "99+" : networkNotificationCount})
       </button>
-      <button>Jobs ()</button>
-      <button>Messaging ()</button>
-      <button>Notification ()</button>
+      <button>Jobs ({jobsCount})</button>
+      <button>Messaging ({messageCount})</button>
+      <button>Notification ({notificationCount})</button>
       <button>Me</button>
     </>
   );
