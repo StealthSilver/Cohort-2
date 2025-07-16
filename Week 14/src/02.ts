519c419fccc35a5ece56c7e204be37a078302593
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-07-16 13:31:23 +0530

    typescript

--- Week 14/src/02.ts
+++ Week 14/src/02.ts
@@ -4,4 +4,10 @@ function greet(firstName: String){
     console.log("hello " + firstName);
 }
 
+function greet2(secondName: any){
+    console.log("hello" + secondName)
+}
+
 greet("pogo");
+
+greet2(423);
