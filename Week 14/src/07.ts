db0c59386c94209ea8c2aff138740dd562a9015a
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-07-16 13:50:24 +0530

    typescript

--- Week 14/src/07.ts
+++ Week 14/src/07.ts
@@ -4,7 +4,7 @@ function greetUser(user: { name: string; age: number }) {
     console.log("Hello " + user.name + user.age) ;
 }
 
-
+// types will be automatically infered for the object
 greetUser({
     name: "pogo",
     age: 87
