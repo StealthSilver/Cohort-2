1a7dda49a0e18dc26866e3cef3106cbb0466bbd7
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-07-16 14:02:40 +0530

    typescript

--- Week 14/01-basics/src/09.ts
+++ Week 14/01-basics/src/09.ts
@@ -15,3 +15,8 @@ interface Employee {
     startDate: new Date(),
     department: "Software developer"
   };
+
+// ts can sum num + num 
+// ts can sum string + string
+// ts can sum string  + num 
+// ts cannot sum (string | num) + (string | num)
