6194570e99210b2b85abfd4d1e61df1c72cfa272
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-07-16 13:37:26 +0530

    typescript

--- Week 14/src/06.ts
+++ Week 14/src/06.ts
@@ -1,9 +1,11 @@
 // function that runs another function after 1 sec
 // in this case the type of the callback function will be void as the callback does not return a value
-function delayedCall(fn: () => void ){
-    setTimeout(fn, 1000);
+function delayedCall(anotherFn: () => void ){
+    setTimeout(anotherFn, 1000);
 }
 
-delayedCall(function(){
-    console.log("hello")
-})
+function log(){
+    console.log("hi there");
+}
+
+delayedCall(log);
