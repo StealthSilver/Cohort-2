971a5918c4ba07cf2ee0a747abc3d8102e4b6cd5
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-07-30 12:41:54 +0530

    interface

--- Week 14/01-basics/src/15.ts
+++ Week 14/01-basics/src/15.ts
@@ -1,10 +1,22 @@
 // types
 
+type Name = { name: string };
+type Age = { age: number };
 
-type User8 = {
-    name: string;
-    age: number
-}
-function isLegal(user: User8){
-return user.age > 18
+// Intersection
+type Person = Name & Age;
+
+type Admin = Person & { role: "admin" };
+type Guest = Person & { access: "limited" };
+
+// Union
+type User = Admin | Guest;
+
+function showUserInfo(user: User) {
+  console.log(`Name: ${user.name}, Age: ${user.age}`);
+  if ("role" in user) {
+    console.log("Admin access");
+  } else {
+    console.log("Guest access");
+  }
 }
