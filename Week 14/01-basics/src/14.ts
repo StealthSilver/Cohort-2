f9bd208f0301ec62bb19302ad4330cb728154d7f
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-07-30 12:01:49 +0530

    interface

---
+++ Week 14/01-basics/src/14.ts
@@ -0,0 +1,26 @@
+// abstract classes 
+
+abstract class User9 {
+    name: string;
+    constructor(name: string) {
+        this.name = name;
+    }
+
+    abstract greet(): string; 
+
+    hello(){
+        console.log("hi there");
+    }
+}
+
+class Employee extends User9 {
+    name: string;
+    constructor(name: string){
+        super(name)
+        this.name = name;
+    }
+
+    greet(){
+        return "hi" + this.name;
+    }
+}
