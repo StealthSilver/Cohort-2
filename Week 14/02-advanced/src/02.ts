623ed3366cd1bc385557165c6b7b54850271e0c6
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-07-30 13:21:19 +0530

    interface

--- Week 14/02-advanced/src/02.ts
+++ Week 14/02-advanced/src/02.ts
@@ -5,3 +5,9 @@ interface User2{
     email: string;
     password: string;
 }
+
+type UpdateProps = Pick<User2, 'name' | 'age' | 'email'>
+
+function updateUser(UpdateProps: UpdateProps){
+    // hit the database tp update the user
+}
