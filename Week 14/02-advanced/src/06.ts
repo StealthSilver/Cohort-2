11c51225c8998ac606fc543e26e80b69d310397f
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-07-31 11:43:13 +0530

    blogs to write

--- Week 14/02-advanced/src/06.ts
+++ Week 14/02-advanced/src/06.ts
@@ -1,7 +1,9 @@
 // map, a javascript concept
 
-const usersMap = new Map<string, { age: number; name: string }>([
-    ['4248j', { age: 32, name: 'pop' }],
-    ['4248j32', { age: 31, name: 'sdf' }],
-  ]);
-  
+const users3 = new Map<string, { age: number; name: string }>();
+
+users3.set('4248j', { age: 32, name: 'pop' });
+users3.set('4248j32', { age: 31, name: 'sdf' });
+
+
+console.log(users3.get('4248j')); // { age: 32, name: 'pop' }
