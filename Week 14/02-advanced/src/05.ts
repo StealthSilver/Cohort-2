71e5995a7bcb7e7ccd5a23694e197c04b61a5931
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-07-31 11:40:51 +0530

    blogs to write

--- Week 14/02-advanced/src/05.ts
+++ Week 14/02-advanced/src/05.ts
@@ -6,10 +6,10 @@
 //     [key : string] : string;
 // }
 
-type Users6 = Record<string ,number>
+type Users6 = Record<string ,{age:number, name: string}>
 
 const users: Users6 =  {
-    '4248j' :12
+    '4248j' :{age: 32, name: "pop"}
     ,
-    '4248j32' :32
+    '4248j32' :{age:31, name:"sdf"}
     }
