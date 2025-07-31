3f9da91b645b67ca3189fd7aa8d1947364b07c54
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-07-31 11:35:05 +0530

    blogs to write

--- Week 14/02-advanced/src/04.ts
+++ Week 14/02-advanced/src/04.ts
@@ -2,3 +2,15 @@
 
 const a = [1,2,3];
 a[0] = 4; // we can change the internal values of a, not the refrence of a 
+
+type User4 = {
+    readonly name: string,
+    readonly age :number
+}
+
+const user: User4 = {
+    name: "pop", 
+    age: 90
+}
+
+// user.age = 32 , this cant be done
