38ae51f0f976fc38f2d1935015a2e1632fdfca0c
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-07-31 12:37:13 +0530

    blogs to write

--- Week 15/Brainly/src/index.ts
+++ Week 15/Brainly/src/index.ts
@@ -3,3 +3,33 @@ import mongoose from "mongoose"
 import jwt from "jsonwebtoken"
 
 const app = express();
+
+app.post("/api/vi/signup" , (req,res) => {
+
+})
+
+
+app.post("/api/vi/signin" , (req,res) => {
+    
+})
+
+app.get("/api/vi/content" , (req,res) => {
+    
+})
+
+
+app.post("/api/vi/content" , (req,res) => {
+    
+})
+
+app.delete("/api/vi/content" , (req,res) => {
+    
+})
+
+app.post("/api/vi/brain/share" , (req,res) => {
+    
+})
+
+app.get("/api/vi/brain/:shareLink" , (req,res) => {
+    
+})
