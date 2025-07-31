96c6565c186220c471949954ac0596ae971708f4
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-07-31 13:10:01 +0530

    re

--- Week 15/Brainly/src/db.ts
+++ Week 15/Brainly/src/db.ts
@@ -1,6 +1,27 @@
-import mongoose, {Model , Schema} from 'mongoose';
+import mongoose, { Model, Schema, Types } from 'mongoose';
 
 const userSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-  });
+});
+
+
+const tagSchema = new mongoose.Schema({
+    title: { type: String, required: true, unique: true }
+});
+
+
+const contentTypes = ['image', 'video', 'article', 'audio']; // Extend as needed
+
+const contentSchema = new Schema({
+    link: { type: String, required: true },
+    type: { type: String, enum: contentTypes, required: true },
+    title: { type: String, required: true },
+    tags: [{ type: Types.ObjectId, ref: 'Tag' }],
+    userId: { type: Types.ObjectId, ref: 'User', required: true },
+});
+
+const linkSchema = new mongoose.Schema({
+    hash: { type: String, required: true },
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+});
