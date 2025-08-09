f4a4aafff86205ddf84bd9489160baa895a96b0e
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-08-09 21:20:25 +0530

    app/web

--- Week 21/turborepo/chat-app/apps/web/app/page.tsx
+++ Week 21/turborepo/chat-app/apps/web/app/page.tsx
@@ -1,5 +1,20 @@
 export default function Home(){
   return (
-    <div></div>
+    <div style = {{
+      height: "100vh",
+      width : "100vw",
+      background: "black",
+      display : "flex",
+      justifyItems: "center",
+      justifyContent: "center"
+    }}>
+      <div style = {{
+      display: "flex",
+      justifyItems: "center",
+      justifyContent: "column"
+    }}>
+      <input type="text" />
+      <button>Join room</button>
+    </div></div>
   )
 }
