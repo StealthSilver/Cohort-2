e05ef5393bdd45685f185d070023c551a28d5b89
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-08-10 16:53:06 +0530

    app/web

---
+++ Week 17/src/index.ts
@@ -0,0 +1,10 @@
+import {Client} from "pg";
+
+const pgClient = new Client('postgresql://neondb_owner:npg_i1FznEcw3Aqf@ep-silent-wave-afzqxl71-pooler.c-2.us-west-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require');
+
+async function main(){
+    await pgClient.connect();
+
+    const response = await pgClient.query("SELECT * FROM users;");
+    console.log(response);
+}
