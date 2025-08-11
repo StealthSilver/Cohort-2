a032be9aa951b55d07fccd5821b57141a5deed95
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-08-11 19:25:13 +0530

    heartbeating

--- Week 16/chat app/chat-app-fe/src/App.tsx
+++ Week 16/chat app/chat-app-fe/src/App.tsx
@@ -5,7 +5,11 @@ function App() {
 
   return (
     <div className='h-screen bg-black'>
-      hi there
+      <div className='h-[95vh] bg-red-300'></div>
+      <div className='w-full bg-white flex p-4'>
+        <input className='flex-1' type="text" />
+        <button className='bg-purple-600 text-white'>Send Message</button>
+      </div>
     </div>
   )
 }
