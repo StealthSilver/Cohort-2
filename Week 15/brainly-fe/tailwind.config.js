dee5f44c070f472710cb0576e2f6a561e311f7b6
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-08-11 15:16:38 +0530

    ui component

--- Week 15/brainly-fe/tailwind.config.js
+++ Week 15/brainly-fe/tailwind.config.js
@@ -1,16 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
       colors: {
         purple: {
-          300: "#ebe7fe",
-          500: "#3e38q7",
+          300: "#e0e7fe",
+          500: "#3e38a7",
           600: "#5046e4",
-        },
-      },
+        }
+      }
     },
   },
   plugins: [],
-};
+}
+
