1e50296939aaa37fae58556ff494736c6db8ac8f
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-08-11 17:39:48 +0530

    heartbeating

--- Week 16/chat-app/src/index.ts
+++ Week 16/chat-app/src/index.ts
@@ -5,6 +5,7 @@ let userCount = 0;
 
 
 wss.on("connection", (socket) => {
-    console.log("user connected")
+    
     userCount = userCount + 1;
+    console.log("user connected" + " " + userCount);
 })
