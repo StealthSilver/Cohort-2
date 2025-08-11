46b033a4ea4b0a8ecfb8fd5e655e08132a5f5ea2
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-08-11 18:37:40 +0530

    heartbeating

--- Week 16/chat app/chat-app-be/src/index.ts
+++ Week 16/chat app/chat-app-be/src/index.ts
@@ -3,36 +3,31 @@ import { WebSocketServer, WebSocket } from "ws";
 const wss = new WebSocketServer({ port: 8080 });
 let userCount = 0;
 
-interface User{
+interface User {
   socket: WebSocket;
   room: string;
 }
 
-let allSockets : User[] = [];
+let allSockets: User[] = [];
 
 wss.on("connection", (socket) => {
- 
-
   socket.on("message", (message) => {
     const parsedMessage = JSON.parse(message as unknown as string);
-    if(parsedMessage.type === "join"){
+    if (parsedMessage.type === "join") {
       allSockets.push({
         room: parsedMessage.payload.roomId,
         socket,
-      })
+      });
     }
 
-    if(parsedMessage.type = "chat"){
+    if ((parsedMessage.type = "chat")) {
       // const currentUserRoom = allSockets.find((x) => x.socket == socket).room
       let currentUserRoom = null;
-      for(let i=0; i< allSockets.length; i++){
-        if(allSockets[i].socket == socket){
+      for (let i = 0; i < allSockets.length; i++) {
+        if (allSockets[i].socket == socket) {
           currentUserRoom = allSockets[i].room;
         }
       }
     }
   });
-
-
-  
 });
