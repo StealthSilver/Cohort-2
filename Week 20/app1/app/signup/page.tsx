9c0e509257bc5c5400180657d1ca84af4dc8e1e1
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-08-12 14:33:04 +0530

    components

---
+++ Week 20/app1/app/signup/page.tsx
@@ -0,0 +1,9 @@
+export default function(){
+    return <div>
+        Sign in
+        <br />
+        <input type="text"></input>
+        <input type="text"></input>
+        <button>Sign in</button>
+    </div>
+}
