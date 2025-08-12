1e082ebd26dc45e06b7d959ea81748523803ea0f
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-08-12 11:42:30 +0530

    ops

--- Week 19/app4/app/page.tsx
+++ Week 19/app4/app/page.tsx
@@ -1,20 +1,10 @@
-'use client'
 import axios from "axios"
-
-import { SetStateAction, useEffect, useState } from "react";
 
 
 export default async function User() {
 
   const response = await axios.get("some url");
   const data = response.data;
-
-
-  if(loading){
-return <div>
-  loading...
-</div>
-  }
 
   return (
     <div>
