1833e1387d34cab80b56b86bc6c9f079d9c37279
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-08-12 11:37:46 +0530

    ops

--- Week 19/app3/app/page.tsx
+++ Week 19/app3/app/page.tsx
@@ -1,9 +1,31 @@
-import Image from "next/image";
+'use client'
+import axios from "axios"
 
-export default function Home() {
+import { SetStateAction, useEffect, useState } from "react";
+
+
+export default function User() {
+
+  const [loading, settLoading] = useState(true);
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    axios.get("https://week-13-ofline.kirattechnologies.wrkers.dev/api/v1/users/details").then((response: { data: SetStateAction<undefined>; }) => {
+      setData(response.data)
+    })
+  }, [])
+
+  if(loading){
+return <div>
+  loading...
+</div>
+  }
+
   return (
     <div>
-      Hello
+      User page 
+      {data.name}
+      {data.email}
     </div>
   );
 }
